8319b0f664d6f9efd862fe9f31f033ab4f59af39
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-14 03:01:44 +0900

    [level 0] Title: 수 조작하기 1, Time: 2.99 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,15 @@
+def solution(n, control):
+    
+    sum = n
+    
+    for i in range(0, len(control)):
+        if control[i] == "w":
+            sum += 1
+        elif control[i] == "s":
+            sum -= 1
+        elif control[i] == "d":
+            sum += 10
+        elif control[i] == "a":
+            sum -= 10
+    
+    return sum
