7ad7e1f50483888769c8fa2dd3f6a9cc5452cb7a
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-15 19:25:51 +0900

    [level 0] Title: 수 조작하기 2, Time: 13.40 ms, Memory: 12.8 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,8 +1,17 @@
-def solution(numLog):
+def solution(num):
+    
     result = ""
-    dict = {1:'w', -1:"s", 10:"d", -10:"a"}
     
-    for i in range(1, len(numLog)):
-        result += dict[numLog[i] - numLog[i-1]]
-    
+    for i in range(1, len(num)):
+        temp = num[i] - num[i-1]
+        
+        if temp == 1:
+            result += "w"
+        elif temp == -1:
+            result += "s"
+        elif temp == 10:
+            result += "d"
+        elif temp == -10:
+            result += "a"
     return result
+            
