16bab8335500940c7905d574c2c51262378017f2
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-19 15:32:30 +0900

    [level 0] Title: 배열 만들기 2, Time: 28.57 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,7 @@
+def solution(l, r):
+    result = []
+    for i in range(l, r+1) :
+        if not set(str(i)) - set(['0', '5']) :
+            result.append(i)
+            
+    return result if result else [-1]
