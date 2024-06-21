ce0fd40a02b28e400fdc784db6b6052a37f3103a
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-21 15:17:01 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,3 +1,12 @@
-def solution(l):
-    l.append(l[-1] - l[-2]) if l[-1] > l[-2] else l.append(l[-1]*2)
-    return l
+def solution(num):
+    
+    result = []
+    
+    for i in range(0, len(num)):
+        result.append(num[i])
+    
+    if num[-1] > num[-2]:
+        result.append(num[-1] - num[-2])
+    else:
+        result.append(num[-1]*2)
+    return result
