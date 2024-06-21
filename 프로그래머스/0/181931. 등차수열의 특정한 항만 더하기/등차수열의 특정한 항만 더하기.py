4bf4d00555c1fc668ba69ae3dbac362f07b18033
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-21 13:57:30 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -1,10 +1,13 @@
 def solution(a, d, included):
-    sum = 0
+    temp = 0
+    result= 0
     
-    for i, j in enumerate(included):
-        if j == True:
-            sum += (i*d) + a 
+    for i in range(0, len(included)):
+        temp = a + (d*i)
+        if included[i] == 1:
+            result += temp
             
-    return sum
-        
-        
+    return result
+
+# +=는 변수에 값을 누적시키는 데 사용되고, =는 변수를 특정 값으로 초기화하거나 재할당하는 데 사용
+            
