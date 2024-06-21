ed1fd832f51b0747644d3ca5872f369944f0d981
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-21 16:02:06 +0900

    [level 0] Title: 배열 만들기 4, Time: 4.77 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -1,7 +1,7 @@
 def solution(arr):
     stk = []
     i = 0
-    while i < len(arr):    
+    while i < len(arr):
         if len(stk) == 0:
             stk.append(arr[i])
             i += 1
@@ -9,10 +9,7 @@ def solution(arr):
             if stk[-1] < arr[i]:
                 stk.append(arr[i])
                 i += 1
-            elif stk[-1] >= arr[i]:
+            else:
                 stk.pop()
-    
     return stk
-    
-
-                
+            
