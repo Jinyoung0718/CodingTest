59fb29063a645bbb7d6c792c642e34078f7ab645
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-21 15:27:58 +0900

    [level 0] Title: 수 조작하기 2, Time: 14.32 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -1,10 +1,9 @@
 def solution(num):
     result = ""
-    dict = {1:"w", -1:"s", 10:"d", -10:"a"}
+    dict = {1:'w', -1:'s', 10:'d', -10:'a'}
     
     for i in range(1, len(num)):
         result += dict[num[i] - num[i-1]]
     
     return result
-        
-        
+    
