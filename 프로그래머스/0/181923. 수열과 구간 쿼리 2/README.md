45db1639e34b899197451f6cc90d1f9795fcfabd
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-21 15:44:09 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 7.33 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923#) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 7.36 ms
+메모리: 10.4 MB, 시간: 7.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 15일 16:49:56
+2024년 06월 21일 15:44:07
 
 ### 문제 설명
 
--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,19 +1,18 @@
 def solution(arr, queries):
+    
     result = []
     
-    for query in queries:
-        s, e, k = query
+    for s, e, k in queries:
         temp = []
-        
         for i in range(s, e+1):
             if arr[i] > k:
                 temp.append(arr[i])
-                
         if temp:
             result.append(min(temp))
         else:
             result.append(-1)
-                      
     return result
-                      
-                      
+            
+                
+            
+                
