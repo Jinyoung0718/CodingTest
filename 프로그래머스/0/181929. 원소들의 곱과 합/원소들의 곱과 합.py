7e7af47884ded125b4f1a76c4c01abd7b80f22b3
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-21 15:03:49 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.py
@@ -1,19 +1,8 @@
-def solution(num_list):
-    
-    multiple = 1
-    square  = 0
-    
-    for i in range(0, len(num_list)):
-        multiple *= num_list[i]
-        
-    for i in range(0, len(num_list)):
-        square  += num_list[i]
-    
-    square = square**2
-    
-    if multiple > square:
-        return 0
-    
-    if multiple < square:
-        return 1
-    
+def solution(num):
+    a = 1
+    b = 0
+    for i in num:
+        a *= i
+        b += i
+    b = b**2
+    return 1 if a < b else 0
