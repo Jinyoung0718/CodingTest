3acb6216b7a8f568bb9cdbe346d7d1487fc3d70b
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-21 13:36:32 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/README.md
+++ 프로그래머스/0/181932. 코드 처리하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 코드 처리하기 - 181932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181932?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181932) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 10.4 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 14일 02:37:49
+2024년 06월 21일 13:36:30
 
 ### 문제 설명
 
--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -4,14 +4,18 @@ def solution(code):
     result = ""
     
     for i in range(0, len(code)):
-        if code[i] == "1":
-            mode = 1 - mode
-        if mode == 0 and code[i] != "1":
-            if i % 2 == 0 :
-                result += code[i]
-        elif mode == 1 and code[i] != "1":
-            if i % 2 !=0 :
-                result += code[i]
-                
+        if mode == 0:
+            if code[i] == '1':
+                mode = 1 - mode
+            else:
+                if i % 2 == 0:
+                    result += code[i]
+        else:
+            if code[i] == '1':
+                mode = 1 - mode
+            else:
+                if i % 2 != 0:
+                    result += code[i]
+                    
     return result if result else "EMPTY"
-                
+                    
