2dc0339af202ac4f2d88b3a2f1c2e69d4d3c6dc6
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-24 13:15:10 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,14 +1,3 @@
 def solution(n, k):
     
-    result = []
-    a = int(n/k)
-    for i in range(1, a+1):
-        temp = k*i
-        print(temp)
-        bo = True
-        if temp > n:
-            bo = False
-            break
-        if bo:
-            result.append(temp)
-    return result
+    return [i for i in range(k, n+1, k)]
