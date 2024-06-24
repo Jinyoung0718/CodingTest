13df219fda008c6206c3df11316774203f70205b
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-24 10:50:15 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,6 +1,8 @@
 def solution(my_string, is_suffix):
+    result = ""
     
-    for i in range (len(my_string)):
-        if my_string[i:] == is_suffix:
+    for i in range(0, len(my_string)):
+        result = my_string[-i:]
+        if result == is_suffix:
             return 1
     return 0
