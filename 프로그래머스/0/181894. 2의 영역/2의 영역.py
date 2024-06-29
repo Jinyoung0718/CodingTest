edf0a589ff4517c18fe8e3cf3221310b859f6beb
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-30 00:40:09 +0900

    [level 0] Title: 2의 영역, Time: 0.56 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.py
@@ -1,7 +1,8 @@
 def solution(arr):
-    if 2 not in arr:
-        return [-1]
-    start = arr.index(2)
-    end = len(arr) - arr[::-1].index(2)
     
-    return arr[start:end]
+    if 2 in arr:
+        start = arr.index(2)
+        end = len(arr) - arr[::-1].index(2)
+        return arr[start:end]
+    else:
+        return [-1]
