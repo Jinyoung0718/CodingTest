15473b0973b9d24943778d56e596c6a1d1b53928
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-06-30 18:25:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 40.38 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,14 @@
 def solution(part, comp):
     
-    part.sort()
-    comp.sort()
+    dic = {}
+    sum_hash = 0
     
-    for par, com in zip(part, comp):
-        
-        if par != com:
-            return par
-        
-    return part[-1]
+    for par in part:
+        val = hash(par)
+        dic[val] = par
+        sum_hash += val
+    
+    for com in comp:
+        val = hash(com)
+        sum_hash -= val
+    return dic[sum_hash]
