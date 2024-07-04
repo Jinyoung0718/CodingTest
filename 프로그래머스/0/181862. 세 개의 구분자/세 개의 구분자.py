ba54dff4ca319f4ea2aa7701e263b05a82e4173e
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-07-04 12:44:02 +0900

    [level 0] Title: 세 개의 구분자, Time: 11.75 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,9 +1,5 @@
 def solution(myStr):
-    result = []
     
-    for i in ['a', 'b','c']:
-        myStr = myStr.replace(i, ' ')
-    result = myStr.split()
-    if not result:
-        result = ["EMPTY"]
-    return result
+    for i in ['a', 'b', 'c']:
+        myStr = myStr.replace(i, " ")
+    return  myStr.split() if myStr.split() else ["EMPTY"]
