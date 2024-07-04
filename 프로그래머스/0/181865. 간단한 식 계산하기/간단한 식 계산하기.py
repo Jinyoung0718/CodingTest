10a48c448df1cfa36809df52ac828924e63ee9d4
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-07-04 12:15:17 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -1,10 +1,11 @@
-def solution(binomial):
+def solution(bino):
     
-    a, op, c = binomial.split()
+    a, op, b = bino.split()
     
     if op == "+":
-        return int(a) + int(c)
+        return int(a) + int(b)
     elif op == "-":
-        return int(a) - int(c)
-    elif op == "*":
-        return int(a) * int(c)
+        return int(a) - int(b)
+    else:
+        return int(a) * int(b)
+    
