f9be01bde151e77fc0836ceefa102d3f329588b7
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-07-04 12:16:40 +0900

    [level 0] Title: rny_string, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.py
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -1,3 +1,5 @@
 def solution(rny_string):
-    
-    return rny_string.replace("m", "rn")
+    if "m" in rny_string:
+        return rny_string.replace("m", "rn")
+    else:
+        return rny_string
