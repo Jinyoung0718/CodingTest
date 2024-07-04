780001a875ca0eed758d2072e277aa215e150377
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-07-04 11:35:32 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.py
@@ -1,9 +1,4 @@
 def solution(myString, pat):
-    
     myString = myString.upper()
     pat = pat.upper()
-    
-    if len(myString) < len(pat):
-        return 0
-    
-    return 1 if pat in myString else 0
+    return  1 if pat in myString else 0
