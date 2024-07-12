abd2361ac044a63243e85483ac3ce804821fe070
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-07-13 03:22:36 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,18 +1,19 @@
 def solution(a, b, c, d):
-    arr = [a, b, c, d]
-    cnt = [arr.count(item) for item in arr]
+    lis = [a, b, c, d]
+    cnt = [lis.count(item) for item in lis]
+    print(cnt)
     
-    if max(cnt)==4: return a*1111
-    
-    elif max(cnt)==3:
-        return (10 * (arr[cnt.index(3)]) + (arr[cnt.index(1)]))**2       
-    
-    elif max(cnt)==2:    
+    if max(cnt) == 4:
+        return 1111*a
+    elif max(cnt) == 3:
+        return (10 * lis[cnt.index(3)] + lis[cnt.index(1)])**2
+    elif max(cnt) == 2:
         if 1 in cnt:
-            return arr[cnt.index(1)] * arr[cnt.index(1, cnt.index(1)+1)]
+            return lis[cnt.index(1)] * lis[cnt.index(1, cnt.index(1)+1, 4)]
         else:
-            for item in arr:
-                if a!=item:
-                    return (a+item) * abs(a-item)
-        
-    else: return min(arr)
+            for i in lis:
+                if i != a:
+                    return (a+i) * abs(a-i)
+    else:
+        return min(lis)                
+            
