cbfee06d905ae38410f1c9a2ae9937ab111bd893
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-07-14 16:33:36 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,11 +1,10 @@
 def solution(rank, attend):
-    
-    lis = []
-    
+    result = []
     for i in range(len(attend)):
-        if attend[i]:
-            lis.append((rank[i], i))
+        
+        if attend[i] == 1:
+            result.append((rank[i], i ))
+            
+    result.sort()
+    return 10000 * result[0][1] + 100 * result[1][1] + result[2][1]
     
-    lis.sort()
-    print(lis)
-    return 10000 * lis[0][1] + 100 * lis[1][1] + lis[2][1]
