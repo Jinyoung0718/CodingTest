56a9a9c41ca6efd60fd03c78bd6f6599092a88be
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-08-04 23:43:19 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 208 ms, Memory: 41116 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,14 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-numbers = list(map(int, input().split()))
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+S = [0] * (n + 1)
 
-S = [0] * (N+1)
+S[0] = A[0]
 
-for i in range(1, N+1):
-    S[i] = S[i-1] + numbers[i-1]
+for i in range(1, n+1):
+    S[i] = S[i-1] + A[i - 1]
 
-for _ in range(M):
-    a, b = map(int, input().split())
-    print(S[b] - S[a-1])
+
+for _ in range(m):
+    s, e = map(int, input().split())
+    print(S[e] - S[s-1])
