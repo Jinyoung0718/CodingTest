c047524de1182b4bccb511ced1c3500d601a8369
Jinyoung0718 <121544511+Jinyoung0718@users.noreply.github.com>
2024-08-12 20:59:33 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 50964 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+N = int(input())
+MyQueue = deque()
+
+for i in range(1, N+1):
+    MyQueue.append(i)
+
+while len(MyQueue) > 1:
+    MyQueue.popleft()
+    MyQueue.append(MyQueue.popleft())
+
+print(MyQueue[0])
