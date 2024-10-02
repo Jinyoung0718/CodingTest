3c9863ba5252e2c3d2cc6d803777d98a7551bc86
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-02 19:18:53 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+N = int(input())
+M = int(input())
+
+net = [[] for _ in range(N+1)]
+visited = [False for _ in range(N+1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    net[a].append(b)
+    net[b].append(a)
+
+def bfs():
+    q = deque()
+    count = 0
+    q.append(1)
+    visited[1] = True
+    while q:
+        cur = q.popleft()
+        for i, val in enumerate(net[cur]):
+            if visited[val] == False:
+                q.append(val)
+                visited[val] = True
+                count += 1
+    print(count)
+
+bfs()
