3d3f8e88837842cc8587dff7baf2ec5bbd5096c0
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-02 21:54:37 +0900

    [Silver I] Title: 미로 탐색, Time: 64 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,29 +1,31 @@
 from collections import deque
 
-N, M = map(int, input().split())
-grid = [list(map(int, input())) for _ in range(N)]
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+duration = 0
 
-dx=[-1, 1, 0, 0]
-dy=[0, 0, 1, -1]
+visited = [[False] * m for _ in range(n)]
 
-visited = [[False] * M for _ in range(N)]
+def bfs(x, y, leng):
 
-queue = deque()
-queue.append((0, 0, 1))
-visited[0][0] = True
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, 1, -1]
 
-while queue:
-    cur_x, cur_y, cur_distance =queue.popleft()
+    visited[x][y] = True
+    queue = deque()
+    queue.append((x, y, leng))
+    while queue:
+        cur_x, cur_y, cur_len = queue.popleft()
 
-    if cur_x == N - 1 and cur_y == M - 1:
-        print(cur_distance)
-        break
+        if cur_x == n-1 and cur_y == m-1:
+            print(cur_len)
+            break
+        for i in range(4):
+            next_x = cur_x + dx[i]
+            next_y = cur_y + dy[i]
+            if 0 <= next_x < n and 0 <= next_y < m:
+                if graph[next_x][next_y] == 1 and not visited[next_x][next_y]:
+                    visited[next_x][next_y] = True
+                    queue.append((next_x, next_y, cur_len+1)) 
 
-    for i in range(4):
-        next_x = cur_x + dx[i]
-        next_y = cur_y + dy[i]
-
-        if next_x >= 0 and next_x < N and next_y >= 0 and next_y < M: 
-            if grid[next_x][next_y] == 1 and not visited[next_x][next_y]:
-                visited[next_x][next_y] = True
-                queue.append((next_x, next_y, cur_distance + 1))
+bfs(0, 0, 1)
