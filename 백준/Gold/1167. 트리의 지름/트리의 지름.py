f3a917c706339a3ebde92aa1b243e03a76d978d3
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-08 15:12:14 +0900

    [Gold II] Title: 트리의 지름, Time: 2980 ms, Memory: 72484 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,42 +1,44 @@
 from collections import deque
 
-n = int(input()) 
-tree = [[] for _ in range(n+1)]  
+n = int(input())
+tree = [[] for _ in range(n+1)]
 
 for _ in range(n):
-    data = list(map(int, input().split()))  
-    current_node = data[0]  
-    idx = 1
+    Data = list(map(int, input().split()))
+    currnet_node = Data[0]
+    index = 1
     while True:
-        contect_node = data[idx] 
-        if contect_node == -1: 
+        connect_node = Data[index]
+        if connect_node == - 1:
             break
-        distance = data[idx+1]  
-        tree[current_node].append((contect_node, distance)) 
-        idx += 2  
+        distance = Data[index + 1]
+        tree[currnet_node].append((connect_node, distance))
+        index += 2
 
-def bfs(start_node):
+def bfs(v):
+    visited[v] = True
     queue = deque()
-    queue.append(start_node)
-    visited[start_node] = True
+    queue.append(v)
     while queue:
-        current_node = queue.popleft()
-        for next_node, dist in tree[current_node]:
+        cur_node = queue.popleft()
+        for next_node, dist in tree[cur_node]:
             if not visited[next_node]:
                 visited[next_node] = True
                 queue.append(next_node)
-                distance[next_node] = distance[current_node] + dist
+                distance[next_node] = distance[cur_node] + dist 
 
-distance = [0] * (n + 1)
+
 visited = [False] * (n + 1)
+distance = [0] * (n + 1)
 bfs(1)
 
-farthest_node = 1
-for i in range(2, n+1):
-    if distance[farthest_node] < distance[i]:
-        farthest_node = i
+far_node = 1
 
-distance = [0] * (n + 1)
+for i in range(2, n + 1): # distance 인덱스를 통해 가장 먼 ㄴ드 찾기
+    if distance[far_node] < distance[i]:
+        far_node = i
+
 visited = [False] * (n + 1)
-bfs(farthest_node)
+distance = [0] * (n + 1)
+bfs(far_node) # 가장 먼 노드는 끝쪽 노드이기에 최고 거리를 재면, 지름의 길이가 됨
 print(max(distance))
