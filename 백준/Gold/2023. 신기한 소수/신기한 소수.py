9c1f57d5ab9ee8f92afe3f8c40bc0d740f10ded4
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-08 14:30:17 +0900

    [Gold V] Title: 신기한 소수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,29 +1,25 @@
 import sys
 sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-n = int(input())  # N자리 수
+n = int(input())
 
-# 소수 판별 함수
-def is_Prime(num):
-    if num == 1:  # 1은 소수가 아님
-        return False
-    for i in range(2, int(num**(1/2)) + 1):  # 2부터 제곱근까지 확인
-        if num % i == 0:
-            return False
+def is_prime(num):
+    if num == 1: return False
+    for su in range(2, int(num**(1/2)) + 1):
+        if num % su == 0: return False
     return True
 
-# DFS 탐색 함수
-def dfs(number, length):
-    if length == n:  # N자리 숫자가 완성되면 출력
+def dfs(number):
+    if len(str(number)) == n:
         print(number)
-        return
-    for i in range(1, 10, 2):  # 다음 자리에 홀수만 가능 (짝수는 소수가 아님)
-        new_number = number * 10 + i
-        if is_Prime(new_number):  # 새로운 숫자가 소수이면 계속 탐색
-            dfs(new_number, length + 1)
+    else:
+        for i in range(1, 10, 2):
+            new_number = number * 10 + i
+            if is_prime(new_number):
+                dfs(new_number)
 
-# 한 자리 소수로 시작 (2, 3, 5, 7)
-dfs(2, 1)
-dfs(3, 1)
-dfs(5, 1)
-dfs(7, 1)
+dfs(2)
+dfs(3)
+dfs(5)
+dfs(7)
