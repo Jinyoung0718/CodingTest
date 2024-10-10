4691eaabd6f041235a2ceb7ec2d0088bc58e780a
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-10 15:35:19 +0900

    [Gold V] Title: ABCDE, Time: 1084 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -2,17 +2,16 @@ import sys
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-graph = [[] * n for _ in range(n)]
-visited = [False] * n
+people, relation = map(int, input().split())
+graph = [[] * people for _ in range(people)]
+visited = [False] * (people)
 
 result = False
 
-for _ in range(m):
+for _ in range(relation):
     s, e = map(int, input().split())
     graph[s].append(e)
     graph[e].append(s)
-
 
 def dfs(v, depth):
     global result
@@ -20,20 +19,24 @@ def dfs(v, depth):
     if depth == 4:
         result = True
         return
-    
+
     visited[v] = True
+
     for next_v in graph[v]:
         if not visited[next_v]:
-            dfs(next_v, depth+1)
+            dfs(next_v, depth + 1)
     
     visited[v] = False
-
     return result
 
-for i in range(n):
+
+
+for i in range(people):
     dfs(i, 0)
     if result:
         break
 
-if result: print(1) 
-else: print(0)
+if result:
+    print(1)
+else:
+    print(0)
