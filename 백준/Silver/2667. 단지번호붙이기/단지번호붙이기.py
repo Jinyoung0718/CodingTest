7b5e3098911bc24d263b1f2f4a7e08d050d0f8f3
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-12 20:17:22 +0900

    [Silver I] Title: 단지번호붙이기, Time: 52 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -2,42 +2,35 @@ from collections import deque
 
 n = int(input())
 graph = [list(map(int, input())) for _ in range(n)]
-row = len(graph)
-col = len(graph[0])
-visited = [[False] * col for _ in range(row)]
+visited = [[False] * n for _ in range(n)]
+result = []
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def bfs(x, y):
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
     visited[x][y] = True
-    bfs_result = 1
+    count = 1
     queue = deque()
     queue.append((x, y))
-
     while queue:
         cur_x, cur_y = queue.popleft()
         for i in range(4):
-            next_x =  cur_x + dx[i]
+            next_x = cur_x + dx[i]
             next_y = cur_y + dy[i]
-            if 0 <= next_x < row and 0 <= next_y < col:
-                if not visited[next_x][next_y] and graph[next_x][next_y] == 1:
+            if 0 <= next_x < n and 0 <= next_y < n:
+                if graph[next_x][next_y] != 0 and not visited[next_x][next_y]:
                     visited[next_x][next_y] = True
                     queue.append((next_x, next_y))
-                    bfs_result += 1
+                    count += 1
+    return count
 
-    return bfs_result
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j] and graph[i][j] != 0:
+            result.append(bfs(i, j))
 
-appartment_arr = []
-
-for i in range(row):
-    for j in range(col):
-        if graph[i][j] == 1 and not visited[i][j]:
-            appartment_arr.append(bfs(i, j))
-
-print(len(appartment_arr))
-
-appartment_arr.sort()
-
-for i in appartment_arr:
-    print(i)
+print(len(result))
+result.sort()
+for num in result:
+    print(num)
