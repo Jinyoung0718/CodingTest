2b99c1ac39306d299cc7b499546cd3f9cfb2a9e6
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-13 11:02:45 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 572 ms, Memory: 65248 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -3,34 +3,31 @@ input = sys.stdin.readline
 
 from collections import deque
 
+n ,m = map(int ,input().split())
+graph = [[] * (n + 1) for _ in range(n + 1)]
+visited = [False] * (n + 1)
+count = 0
 
-n, m = map(int, input().split())
-
-graph = [[] * n for _ in range(n + 1)]  
-# 정점 간의 연결을 나타내려면, 정점의 개수(n) 크기를 기준으로 그래프를 구성하는 것
-visited = [False] * (n+1)
-
-for i in range(m):
-    s, e = map(int, input().split())
+for _ in range(m):
+    s, e = map(int ,input().split())
     graph[s].append(e)
     graph[e].append(s)
 
-def bfs(v):
-    visited[v] = True
-    queue = deque([v])
+
+def bfs(cur_v):
+    visited[cur_v] = True 
+    queue = deque()
+    queue.append(cur_v)
     while queue:
         cur_v = queue.popleft()
         for next_v in graph[cur_v]:
             if not visited[next_v]:
                 visited[next_v] = True
-                queue.append(next_v)
-
-
-result = 0
+                queue.append(next_v) 
 
 for i in range(1, n+1):
     if not visited[i]:
         bfs(i)
-        result += 1
+        count += 1
 
-print(result)
+print(count)
