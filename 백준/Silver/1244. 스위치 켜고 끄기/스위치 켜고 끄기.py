a697318a8332c98479a4157dc49fdf83224d1b85
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-17 23:27:45 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -4,28 +4,27 @@ def change(index):
     else:
         switch[index] = 1
 
-n = int(input())
-switch = [-1] + list(map(int, input().split()))
 total = int(input())
+switch = [-1] + list(map(int, input().split()))
+students = int(input())
 
-for _ in range(total):
+for _ in range(students):
     sex, target = map(int, input().split())
-    
+
     if sex == 1:
-        for i in range(target, n + 1, target):
+        for i in range(target, len(switch), target):
             change(i)
-    
     else:
         change(target)
-        for k in range(n//2):
-            if target - k < 1 or target + k > n: break
-            if switch[target + k] == switch[target - k]:
-                change(target + k)
-                change(target - k)
+        for i in range(total//2):
+            if target + i > total or  target - i < 1: break # 범위는 항상 생각하자
+            if switch[target + i] == switch[target - i]:
+                change(target + i)
+                change(target - i)
             else:
                 break
 
-for i in range(1, n + 1):
+for i in range(1, len(switch)):
     print(switch[i], end=" ")
     if i % 20 == 0:
         print()
