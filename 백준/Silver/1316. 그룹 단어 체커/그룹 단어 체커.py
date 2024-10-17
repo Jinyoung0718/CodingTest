7d7cbd12f00b6f1dad0cdf26f8abe0505c17b1cc
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-18 00:58:27 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,23 +1,21 @@
-n = int(input())  # 단어의 개수 입력
-group_word_count = 0  # 그룹 단어의 개수를 세는 변수
+n = int(input())
+result = 0
 
-# 각 단어를 검사
 for _ in range(n):
-    word = input()  # 단어 입력
-    is_group_word = True  # 그룹 단어인지 여부를 나타내는 변수
-    seen = set()  # 등장한 문자를 저장할 집합
-    previous_char = ''  # 이전 문자를 저장할 변수
+    sentence = input()
+    find = True
+    previous = ""
+    seen = set()
 
-    for char in word:
-        if char != previous_char:
-            if char in seen:  # 이미 등장한 문자가 다시 등장하면 그룹 단어가 아님
-                is_group_word = False
+    for i in sentence:
+        if i != previous:
+            if i in seen:
+                find = False
                 break
-            seen.add(char)  # 처음 등장하는 문자를 집합에 추가
-        previous_char = char  # 이전 문자를 현재 문자로 업데이트
+            seen.add(i)
+        previous = i
+    
+    if find:
+        result += 1
 
-    if is_group_word:
-        group_word_count += 1  # 그룹 단어이면 카운트 증가
-
-# 결과 출력
-print(group_word_count)
+print(result)
