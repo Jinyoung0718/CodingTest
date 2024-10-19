3f7cf11627b645b65f48e791abb3d59fefbebe12
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-19 16:58:51 +0900

    [Silver IV] Title: 색종이 - 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,22 +1,23 @@
 n = int(input())
-arr = [[False] * 101 for _ in range(101)]
+arr = [[0] * 101 for _ in range(101)]
+result = 0
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 for _ in range(n):
     x, y = map(int, input().split())
     for i in range(x, x + 10):
         for j in range(y, y + 10):
-            arr[i][j] = True
-
-total = 0
-
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
+            arr[i][j] = 1
 
 for i in range(101):
     for j in range(101):
-        if arr[i][j] == True:
+        if arr[i][j] == 1:
             for k in range(4):
-                next_i, next_y = i + dx[k], j + dy[k]
-                if next_i < 0 or next_i >= 101 or next_y < 0 or next_y >= 101 or arr[next_i][next_y] == False:
-                    total += 1
-print(total)
+                next_i = i + dx[k]
+                next_y = j + dy[k]
+                if arr[next_i][next_y] == 0:
+                    result += 1
+
+print(result)
