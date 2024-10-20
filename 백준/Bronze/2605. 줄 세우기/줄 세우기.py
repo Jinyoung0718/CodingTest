2bcc662e8ad311d6ed2a80d5dc133135083f5337
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-20 18:25:42 +0900

    [Bronze II] Title: 줄 세우기, Time: 48 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Bronze/2605. 줄 세우기/줄 세우기.py
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.py
@@ -1,10 +1,10 @@
 from collections import deque
 
 n = int(input())
-numbers = list(map(int, input().split()))
+data = list(map(int, input().split()))
 queue = deque()
 
 for i in range(n):
-    queue.insert(len(queue) - numbers[i], i + 1)
+    queue.insert(len(queue) - data[i], i + 1)
 
 print(" ".join(map(str, queue)))
