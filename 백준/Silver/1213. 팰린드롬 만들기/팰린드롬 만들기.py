e6e945808ab60d8103681da01ac1ee78db814cc2
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-20 15:40:07 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 56 ms, Memory: 34028 KB -BaekjoonHub

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,29 @@
+from collections import Counter
+word_list = list(input())
+word_list.sort()
+
+word_count = Counter(word_list)
+
+odd_alphabet_count = 0
+odd_alphabet = ''
+
+for alphabet in word_count:
+    if word_count[alphabet] % 2 == 1:
+        odd_alphabet_count += 1
+        odd_alphabet += alphabet
+    if odd_alphabet_count > 1:
+        print("I'm Sorry Hansoo")
+        exit()
+
+answer = ''
+
+for i in range(0, len(word_list), 2):
+    if word_count[word_list[i]] % 2 == 1:
+        word_count[word_list[i]] -= 1
+    else:
+        answer += word_list[i]
+
+tmp = answer[::-1]
+answer += odd_alphabet
+answer += tmp
+print(answer)
