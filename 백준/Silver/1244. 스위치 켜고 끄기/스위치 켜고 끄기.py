a790332a17c43b1fe7ffde46a66b0ad43a331cc5
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-20 16:32:12 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -6,9 +6,9 @@ def change(index):
 
 total = int(input())
 switch = [-1] + list(map(int, input().split()))
-students = int(input())
+student = int(input())
 
-for _ in range(students):
+for _ in range(student):
     sex, target = map(int, input().split())
 
     if sex == 1:
@@ -16,15 +16,16 @@ for _ in range(students):
             change(i)
     else:
         change(target)
-        for i in range(total//2):
-            if target + i > total or  target - i < 1: break # 범위는 항상 생각하자
-            if switch[target + i] == switch[target - i]:
-                change(target + i)
+        i = 1
+        while target - i > 0 and target + i <= total:
+            if switch[target - i] == switch[target + i]:
                 change(target - i)
+                change(target + i)
             else:
                 break
-
+            i += 1
+            
 for i in range(1, len(switch)):
-    print(switch[i], end=" ")
+    print(switch[i], end= " ")
     if i % 20 == 0:
         print()
