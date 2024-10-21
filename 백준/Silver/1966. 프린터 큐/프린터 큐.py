2473f9e97032e19639c832e3edfa601898853817
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-21 15:21:16 +0900

    [Silver III] Title: 프린터 큐, Time: 68 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,24 +1,20 @@
 from collections import deque
 
-T = int(input())  # 테스트 케이스 수 입력
-for _ in range(T):
-    total, target = map(int, input().split())  # 문서의 개수와 타겟 문서 위치 입력
-    queue = deque(enumerate(map(int, input().split())))  # 중요도와 문서의 인덱스를 저장
+t = int(input())
 
-    count = 0  # 인쇄 순서를 카운트
+for _ in range(t):
+    total, target = map(int, input().split())
+    queue = deque(enumerate(map(int, input().split())))
+    count = 0
 
     while queue:
-        # 현재 큐에서 가장 큰 중요도를 구한다
-        current_max_priority = max([doc[1] for doc in queue]) 
-
-        # 첫 번째 문서를 꺼내 확인
-        current_doc = queue.popleft()
+        max_important = max([doc[1] for doc in queue])
+        current_data = queue.popleft()
         
-        if current_doc[1] == current_max_priority:  # 가장 큰 중요도라면
-            count += 1  # 인쇄 순서를 증가시킨다
-            
-            if current_doc[0] == target:  # 타겟 문서라면
-                print(count)  # 현재 순서를 출력하고 종료
+        if current_data[1] == max_important:
+            count += 1
+            if current_data[0] == target:
+                print(count)
                 break
         else:
-            queue.append(current_doc)  # 중요도가 낮다면 다시 큐에 넣는다
+            queue.append(current_data)
