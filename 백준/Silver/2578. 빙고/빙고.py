92948d2a4f27a62a5b39211e9fdd5f907ea0ea7e
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-22 22:50:58 +0900

    [Silver IV] Title: 빙고, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,55 +1,42 @@
-import sys
-input = sys.stdin.readline
+def check():
+    answer = 0
 
-c = [list(map(int, input().split())) for _ in range(5)]
+    for row in bingo:
+        if row.count(0) == 5:
+            answer += 1
+    
+    for i in range(5):
+        cnt = 0
+        for j in range(5):
+            if bingo[j][i] == 0:
+                cnt += 1
+        if cnt == 5:
+            answer += 1
+    
+    if all(bingo[i][i] == 0 for i in range(5)):
+        answer += 1
+    if all(bingo[i][4 - i] == 0 for i in range(5)):
+        answer += 1
+
+    return answer
+
+
+bingo = [list(map(int, input().split())) for _ in range(5)]
 mc = []
-for _ in range(5):
+
+for i in range(5):
     mc += list(map(int, input().split()))
 
-def check():
-    bingo = 0
-    # 가로 확인
-    for x in c: 
-        if x.count(0) == 5:
-            bingo += 1
-
-    # 세로 확인
-    for i in range(5):
-        y = 0
-        for j in range(5):
-            if c[j][i] == 0:
-                y += 1
-        if y == 5:
-            bingo += 1
-
-    # 왼쪽 위에서 오른쪽 아래로 대각선 확인
-    d1 = 0
-    for i in range(5):
-        if c[i][i] == 0:
-            d1 += 1
-    if d1 == 5:
-        bingo += 1
-
-    # 오른쪽 위에서 왼쪽 아래로 대각선 확인
-    d2 = 0
-    for i in range(5):
-        if c[i][4-i] == 0:
-            d2 += 1
-    if d2 == 5:
-        bingo += 1
-
-    return bingo 
-
-cnt = 0
+count = 0
 for i in range(25):
     for x in range(5):
         for y in range(5):
-            if mc[i] == c[x][y]:
-                c[x][y] = 0  
-                cnt += 1  
-
-    if cnt >= 12:
-        result = check()  
+            if mc[i] == bingo[x][y]:
+                bingo[x][y] = 0
+                count += 1
+    
+    if count >= 12:
+        result = check()
         if result >= 3:
             print(i + 1)
             break
