6c2ce32f358a0b0a522effb5ff2f156a01950d2f
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-10-22 18:07:57 +0900

    [Silver III] Title: 킹, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1063. 킹/킹.py
+++ 백준/Silver/1063. 킹/킹.py
@@ -1,19 +1,18 @@
-dict = {"R": [1, 0], "L": [-1, 0], "B": [0, -1], "T": [0, 1], "RT": [1, 1], "LT": [-1, 1],
-        "RB": [1, -1], "LB": [-1, -1]}
-    
-king, stone, n = input().split()
+dict = {
+    'R': [1, 0], 'L': [-1, 0], 'B': [0, -1], 'T': [0, 1], 'RT': [1, 1],
+    'LT': [-1, 1], 'RB': [1, -1], 'LB': [-1, -1]
+}
 
+king, stone, n = input().split()
 king_loc = list(map(int, [ord(king[0]) - 64, int(king[1])]))
-stone_loc = list(map(int, [ord(stone[0]) - 64, int(stone[1])]))
+stone_loc = list(map(int,[ord(stone[0]) - 64, int(stone[1])]))
 
 for _ in range(int(n)):
     move = input()
-
     next_king_x = king_loc[0] + dict[move][0]
     next_king_y = king_loc[1] + dict[move][1]
-
     if 1 <= next_king_x <= 8 and 1 <= next_king_y <= 8:
-        if stone_loc[0] == next_king_x and stone_loc[1] == next_king_y:
+        if next_king_x == stone_loc[0] and next_king_y == stone_loc[1]:
             next_stone_x = stone_loc[0] + dict[move][0]
             next_stone_y = stone_loc[1] + dict[move][1]
             if 1 <= next_stone_x <= 8 and 1 <= next_stone_y <= 8:
