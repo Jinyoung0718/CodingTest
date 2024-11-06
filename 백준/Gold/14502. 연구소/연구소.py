1cc0f695cdfcd2de34b7fedeca4e4076c30f97bd
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-11-07 01:25:54 +0900

    [Gold IV] Title: 연구소, Time: 2256 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -2,34 +2,36 @@ from itertools import combinations
 from collections import deque
 import copy
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-empty_spaces = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 0]
-virus_positions = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 2]
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+result = 0
 
-def bfs(graph_copy):
-    queue = deque(virus_positions)
+empty_space = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 0]
+virus_space = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 2]
+
+def bfs(graph):
+    queue = deque(virus_space)
     while queue:
-        x, y = queue.popleft()
+        cur_x, cur_y = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and graph_copy[nx][ny] == 0:
-                graph_copy[nx][ny] = 2
-                queue.append((nx, ny))
-    return sum(row.count(0) for row in graph_copy)
+            next_x = cur_x + dx[i]
+            next_y = cur_y + dy[i]
+            if 0 <= next_x < n and 0 <= next_y < m and graph[next_x][next_y] == 0:
+                graph[next_x][next_y] = 2
+                queue.append((next_x, next_y))
 
-max_safe_area = 0
+    return sum(row.count(0) for row in graph)
 
-for walls in combinations(empty_spaces, 3):
-
-    graph_copy = copy.deepcopy(graph)
+temp = 0
+for walls in combinations(empty_space, 3):
+    copy_graph = copy.deepcopy(graph)
     for x, y in walls:
-        graph_copy[x][y] = 1
-    safe_area = bfs(graph_copy)
-    max_safe_area = max(max_safe_area, safe_area)
+        copy_graph[x][y] = 1
 
-print(max_safe_area)
+    temp = bfs(copy_graph)
+    result = max(temp, result)
+
+print(result)
