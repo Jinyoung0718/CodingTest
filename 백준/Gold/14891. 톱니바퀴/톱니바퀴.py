c613dc397e599557e8046394df1823fc306d76c5
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-11-10 19:07:18 +0900

    [Gold V] Title: 톱니바퀴, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,36 +1,35 @@
-N = 4       # 톱니바퀴 4개 (일반화 시키는 것이 연습에 유용)
-arr = [[0]*8]+[list(map(int,input())) for _ in range(N)]
-K = int(input())
-top = [0]*(N+1)
+n = 4
+graph = [[0] * 8] + [list(map(int, input())) for _ in range(n)]
+k = int(input())
+top = [0] * (n + 1)
+result = 0
 
-for _ in range(K):      # K번 idx, dr 입력 받음(회전 명령)
-    idx, dr = map(int, input().split())
-    # [1] idx 톱니를 회전
-    tlst = [(idx, 0)]
-    # [2] idx 우측방향 처리(같은 극성 나오면 탈출)
-    for i in range(idx+1, N+1):
-        # 왼쪽 3시 극성 != 오른쪽 9시 극성 => 회전후보 추가
-        if arr[i-1][(top[i-1]+2)%8]!= arr[i][(top[i]+6)%8]:
-            tlst.append((i, (i-idx)%2))
-        else:           # 같은극성이면 더 이상 전달 안 됨
-            break
-    # [3] idx 좌측방향 처리(같은 극성 나오면 탈출)
-    for i in range(idx-1, 0, -1):
-        # 왼쪽 3시 극성 != 오른쪽 9시 극성 => 회전후보 추가
-        if arr[i][(top[i]+2)%8]!= arr[i+1][(top[i+1]+6)%8]:
-            tlst.append((i, (idx-i)%2))
-        else:           # 같은극성이면 더 이상 전달 안 됨
-            break
-    # [4] 실제 회전 처리(cw 이면 top값을 -1)
-    for i, rot in tlst:
-        if rot==0:      # idx 톱니의 dr과 같은 방향
-            top[i] = (top[i]-dr+8)%8
+for _ in range(k):
+    target, direction = map(int, input().split())
+    temp = [(target, 0)]
+
+    for i in range(target + 1, n+1):
+        if graph[i-1][(top[i-1] + 2) % 8] != graph[i][(top[i] + 6) % 8]:
+            temp.append((i, (i - target) % 2))
         else:
-            top[i] = (top[i]+dr+8)%8
+            break
+    
+    for i in range(target-1, 0, -1):
+        if graph[i+1][(top[i+1] + 6) % 8] != graph[i][(top[i] + 2) % 8]:
+            temp.append((i, (target - i) % 2))
+        else:
+            break
+    
+    
+    for index, rotate in temp:
+        if rotate == 0:
+            top[index] = (top[index] - direction + 8) % 8
+        else:
+            top[index] = (top[index] + direction + 8) % 8
 
-# 점수를 계산
-ans = 0
-tbl = [0, 1, 2, 4, 8]
-for i in range(1, N+1):
-    ans += arr[i][top[i]]*tbl[i]
-print(ans)
+cal_list = [0, 1, 2, 4, 8]
+
+for i in range(1, n+1):
+    result += graph[i][top[i]] * cal_list[i]
+
+print(result)
