c08b90de3871027e6133c28044c432058698e4e0
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-11-10 17:15:27 +0900

    [Gold IV] Title: 연구소, Time: 1488 ms, Memory: 34220 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,5 +1,5 @@
-from itertools import combinations
 from collections import deque
+from itertools import combinations
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
@@ -18,18 +18,16 @@ def bfs(graph):
         for i in range(4):
             next_x = cur_x + dx[i]
             next_y = cur_y + dy[i]
-            if 0 <= next_x < n and 0 <= next_y < m and graph[next_x][next_y] == 0:
+            if 0 <= next_x < n and 0 <= next_y < m and graph[next_x][next_y] ==  0:
                 graph[next_x][next_y] = 2
                 queue.append((next_x, next_y))
+    
+    return sum([row.count(0) for row in graph])
 
-    return sum(row.count(0) for row in graph)
-
-temp = 0
 for walls in combinations(empty_space, 3):
-    copy_graph = [row[:] for row in graph]
+    copy_graph = [row[::] for row in graph]
     for x, y in walls:
         copy_graph[x][y] = 1
-
     temp = bfs(copy_graph)
     result = max(temp, result)
 
