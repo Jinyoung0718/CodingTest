5d7ea645a179add0209495920ff7c35feda25de8
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-11-13 13:30:35 +0900

    [Gold IV] Title: N-Queen, Time: 6608 ms, Memory: 121100 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,23 +1,20 @@
-n = int(input())
-graph = [[0] * n for _ in range(n)]
-result = 0
-v1 = [0] * (n +  1)
-v2 = [0] * (2 * (n + 1))
-v3 = [0] * (2 * (n + 1))
-
 def dfs(depth):
-    global result
+    global  result
 
     if depth == n:
         result += 1
         return
-    
-    for i in range(n):
+
+    for i in range(1, n+1):
         if v1[i] == v2[depth + i] == v3[depth - i] == 0:
             v1[i] = v2[depth + i] = v3[depth - i] = 1
             dfs(depth + 1)
             v1[i] = v2[depth + i] = v3[depth - i] = 0
 
-
+n = int(input())
+result = 0
+v1 = [0] * (n + 1)
+v2 = [0] * (2 * n)
+v3 = [0] * (2 * n)
 dfs(0)
 print(result)
