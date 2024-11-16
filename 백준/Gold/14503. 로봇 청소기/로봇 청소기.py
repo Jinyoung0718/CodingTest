02966d3d42f2a15d3294e7764dd77e11cae4969b
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-11-16 19:01:36 +0900

    [Gold V] Title: 로봇 청소기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -1,32 +1,29 @@
-n, m = map(int, input().split())
-r, c, d = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
-
-dx = [-1, 0, 1, 0] 
+dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-visited[r][c] = 1
-count = 1
+n, m = map(int, input().split())
+x, y, direct = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+graph[x][y] = -1
+result = 1
 
 while True:
-    flag = 0
-    for _ in range(4):
-        d = (d + 3) % 4
-        next_r = r + dx[d]
-        next_c = c + dy[d]
-        if 0 <= next_r < n and 0 <= next_c < m and graph[next_r][next_c] == 0:
-            if visited[next_r][next_c] == 0:
-                visited[next_r][next_c] = 1
-                count += 1
-                r = next_r
-                c = next_c
-                flag = 1
-                break
-    
-    if flag == 0:
-        if graph[r - dx[d]][c - dy[d]] == 1:
-            print(count)
+    clean = False
+    for i in range(4):
+        direct = (direct + 3) % 4
+        next_x = x + dx[direct]
+        next_y = y + dy[direct]
+        if 0 <= next_x < n and 0 <= next_y < m and graph[next_x][next_y] == 0:
+            graph[next_x][next_y] = -1
+            clean = True
+            result += 1
+            x, y = next_x, next_y
             break
+
+    if not clean:
+        if graph[x - dx[direct]][y - dy[direct]] != 1:
+            x -= dx[direct]
+            y -= dy[direct]
         else:
-            r, c = r - dx[d], c - dy[d]
+            print(result)
+            break
