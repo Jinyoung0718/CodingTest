afb6b7242b5f2fbbd2579f1aee8512096c6be9cc
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-12-24 06:39:42 +0900

    [level 2] Title: 전화번호 목록, Time: 568.04 ms, Memory: 46.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,13 +1,16 @@
-def solution(phone_book): 
-    hash_map = {} 
-    for nums in phone_book: 
-        hash_map[nums] = 1 
+def solution(phone_book):
+    memo = {}
     
-    for nums in phone_book: 
-        arr = "" 
-        for num in nums: 
-            arr += num
-            if arr in hash_map and arr != nums:       
-                return False 
-                
+    # 전화번호를 해시 테이블에 저장
+    for num in phone_book:
+        memo[num] = True
+    
+    # 각 전화번호의 접두사를 검사
+    for num in phone_book:
+        temp = ""
+        for i in range(len(num)):
+            temp += num[i]
+            if temp in memo and temp != num:
+                return False
+    
     return True
