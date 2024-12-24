e4c94242cd6c9dc98267e32a1fd78e868d991cb0
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-12-24 15:53:23 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 56 ms, Memory: 34020 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,29 +1,19 @@
 from collections import Counter
-word_list = list(input())
-word_list.sort()
 
-word_count = Counter(word_list)
+word_check = Counter(input())
+result = ""
+odd_alpha = ""
+odd_check = 0
 
-odd_alphabet_count = 0
-odd_alphabet = ''
+for key, val in word_check.items():
+    if val % 2 != 0:
+        odd_alpha = key
+        odd_check += 1
+        if odd_check > 1:
+            print("I'm Sorry Hansoo")
+            exit()
 
-for alphabet in word_count:
-    if word_count[alphabet] % 2 == 1:
-        odd_alphabet_count += 1
-        odd_alphabet += alphabet
-    if odd_alphabet_count > 1:
-        print("I'm Sorry Hansoo")
-        exit()
+for key, val in sorted(word_check.items()):
+    result += (key * (val//2))
 
-answer = ''
-
-for i in range(0, len(word_list), 2):
-    if word_count[word_list[i]] % 2 == 1:
-        word_count[word_list[i]] -= 1
-    else:
-        answer += word_list[i]
-
-tmp = answer[::-1]
-answer += odd_alphabet
-answer += tmp
-print(answer)
+print(result + odd_alpha + result[::-1])
