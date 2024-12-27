3a5052abb38da4f3490f19a35233562138167338
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-12-28 03:29:03 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,19 +1,16 @@
-# 크로아티아 알파벳 리스트
-memo = ['dz=', 'lj', 'nj', 'c=', 'c-', 'd-', 's=', 'z=']
-
+memo = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 word = input()
-count = 0
 i = 0
+answer = 0
 
-# 문자열 탐색 (while 반복문 사용)
 while i < len(word):
     for m in memo:
-        if word[i:i+len(m)] == m:  # 현재 위치에서 패턴 일치 확인
-            count += 1
-            i += len(m)  # 패턴 길이만큼 건너뛰기
+        if word[i:i+len(m)] == m:
+            answer += 1
+            i += len(m)
             break
-    else:  # 어떤 패턴도 매칭되지 않은 경우
-        count += 1
-        i += 1  # 다음 문자로 이동
+    else:
+        answer += 1
+        i += 1
 
-print(count)
+print(answer)
