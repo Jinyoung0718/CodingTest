9ebbc3406d01913066f2236f55f7155b1398592c
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-12-28 00:02:22 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -2,20 +2,19 @@ n = int(input())
 result = 0
 
 for _ in range(n):
-    sentence = input()
-    find = True
-    previous = ""
+    word = input()
     seen = set()
+    previous = ""
+    find = True
 
-    for i in sentence:
-        if i != previous:
-            if i in seen:
+    for w in word:
+        if previous != w:
+            if w in seen:
                 find = False
                 break
-            seen.add(i)
-        previous = i
+            seen.add(w)
+        previous = w
     
-    if find:
-        result += 1
+    if find: result += 1
 
 print(result)
