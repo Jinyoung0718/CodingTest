c53df62f559d018ba84f7d997d022b155f47afe9
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2024-12-31 02:26:18 +0900

    [Bronze II] Title: 농구 경기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.py
+++ 백준/Bronze/1159. 농구 경기/농구 경기.py
@@ -1,16 +1,14 @@
 n = int(input())
 memo = {}
 
-for i in range(n):
-    word = input()
-    if word[0] in memo:
-        memo[word[0]] += 1
-        continue
-    memo[word[0]] = 1
+for _ in range(n):
+    name = input()
+    memo[name[0]] = memo.get(name[0], 0) + 1
 
-result = [key for key, value in memo.items() if value >= 5]
-result.sort()
-if result:
-    print("".join(map(str, result)))
-else:
+answer = [key for key, value in memo.items() if value >= 5]
+answer.sort()
+
+if answer:
+    print("".join(map(str, answer)))
+elif len(answer) < 5:
     print("PREDAJA")
