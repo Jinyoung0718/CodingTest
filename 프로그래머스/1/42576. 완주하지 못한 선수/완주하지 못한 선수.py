ddcf43c5aa293f7b1b2f260f48f1eb9306400a52
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-02-01 19:27:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.92 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,11 @@
 def solution(part, comple):
     memo = {}
     
-    for p in part:
-        if p in memo:
-            memo[p] += 1
-            continue
-        memo[p] = 1
+    for n in part:
+        memo[n] = memo.get(n, 0) + 1
     
-    for c in comple:
-        if c in memo:
-            memo[c] -= 1
+    for n in comple:
+        memo[n] -= 1
     
-    return "".join(map(str, [key for key, value in memo.items() if value == 1]))
+    answer = [key for key, value in memo.items() if value == 1]
+    return answer[0]
