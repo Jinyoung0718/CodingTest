a91860145f0ac678848bbd341102242fe4c606cf
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-02-05 12:04:48 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,26 +1,21 @@
 word = input()
+memo = {}
 
-freq = {}
-for char in word:
-    freq[char] = freq.get(char, 0) + 1
+for n in word:
+    memo[n] = memo.get(n, 0) + 1
 
-mid = ''  # 중앙에 올 문자
-flag = 0  # 홀수 개 문자 개수 카운트
-for char in freq:
-    if freq[char] % 2 == 1:
-        mid = char
+mid = ""
+flag = 0
+
+for key in memo:
+    if memo[key] % 2 != 0:
+        memo[key] -= 1
+        mid = key
         flag += 1
-        freq[char] -= 1
     if flag == 2:
         print("I'm Sorry Hansoo")
         exit()
 
-# 팰린드_롬 구성
-result = []  # 앞쪽 절반
-for char in sorted(freq.keys()):  # 사전 순서로 정렬
-    result.append(char * (freq[char] // 2))  # 짝수 개 문자 절반 추가
-
-# 최종 팰린드_롬 생성
-first_half = ''.join(result)
-palindrome = first_half + mid + first_half[::-1]  # 중앙 문자와 뒷부분 추가
-print(palindrome)
+first_half = "".join(key * (memo[key] // 2) for key in sorted(memo.keys()))
+answer = first_half + mid + first_half[::-1]
+print(answer)
