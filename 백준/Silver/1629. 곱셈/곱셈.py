3784ab79d636ac56d5f20a343f3974bbe187b823
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-02-06 16:17:05 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,14 +1,19 @@
-def mod_exp(a, b, c):
-    if b == 1:
+def mod_exp(a, b):
+    if b == 1:  # 종료 조건: b가 1일 때
         return a % c
 
-    result = mod_exp(a, b//2, c)
-    result = (result * result) % c
+    # 재귀적으로 b를 절반으로 나눔
+    result = mod_exp(a, b // 2)
+    result = (result * result) % c  # 중간 결과에 mod 연산 적용
 
-    if b % 2 == 1:
+    if b % 2 == 1:  # b가 홀수라면 한 번 더 곱해줌
         result = (result * a) % c
 
     return result
 
-A, B, C = map(int, input().split())
-print(mod_exp(A,B,C))
+
+# 입력
+a, b, c = map(int, input().split())
+
+# 출력
+print(mod_exp(a, b))
