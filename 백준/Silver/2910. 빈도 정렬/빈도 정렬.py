690bc2f39d93083099ba6f01d3adc58c9ef7027e
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-02-06 17:02:58 +0900

    [Silver III] Title: 빈도 정렬, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.py
@@ -1,15 +1,15 @@
 n, c = map(int, input().split())
 arr = list(map(int, input().split()))
+answer = []
 
 memo = {}
-answer = []
 
 for num in arr:
     memo[num] = memo.get(num, 0) + 1
 
-sorted_memo = sorted(memo.items(), key=lambda x: (-x[1], arr.index(x[0])))
+sort_memo = sorted(memo.items(), key=lambda x: (-x[1]))
 
-for key, freq in sorted_memo:
+for key, freq in sort_memo:
     answer.extend([key] * freq)
 
 print(*answer)
