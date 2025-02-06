89848d5f7f335824bedbce443498264dfb53ec4c
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-02-06 17:28:51 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,38 +1,33 @@
-def is_acceptable(word):
-    vowels = {'a', 'e', 'i', 'o', 'u'}
+def check_word(word):
+    dic = {'a', 'e', 'i', 'o', 'u'}
+    vowel = 0
+    consonant = 0
+    has_vowel = False
+    prev = ""
 
-    has_vowel = False  # 1번 조건: 모음이 하나 라도 있는지 확인
-    consecutive_vowel = 0  # 2번 조건: 연속된 '모음' 개수
-    consecutive_consonant = 0  # 2번 조건: 연속된 '자음' 개수
-
-    prev_char = ''  # 3번 조건: 연속된 같은 문자 체크
-
-    for char in word:
-        if char in vowels:
+    for n in word:
+        if n in dic:
             has_vowel = True
-            consecutive_vowel += 1
-            consecutive_consonant = 0  # 자음 연속 개수 초기화
+            vowel += 1
+            consonant = 0
         else:
-            consecutive_consonant += 1
-            consecutive_vowel = 0  # 모음 연속 개수 초기화
+            consonant += 1
+            vowel = 0
 
-        # 2. 모음/자음이 3개 연속이면 불가능한 비밀번호
-        if consecutive_vowel >= 3 or consecutive_consonant >= 3:
+        if consonant >= 3 or vowel >= 3:
             return False
 
-        # 3. 같은 글자가 연속으로 두 번 오는지 체크 (ee, oo는 허용)
-        if prev_char == char and char not in {'e', 'o'}:
+        if prev == n and n not in {'e', 'o'}:
             return False
-        
-        prev_char = char
-        
-    return has_vowel  # 모음이 하나라도 있어야 True 반환
 
+        prev = n
+    return has_vowel
 while True:
-    word = input().strip()
+    word = input()
     if word == 'end':
         break
-    if is_acceptable(word):
+
+    if check_word(word):
         print(f'<{word}> is acceptable.')
     else:
         print(f'<{word}> is not acceptable.')
