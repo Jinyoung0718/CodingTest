35c060f507d74a0514bf3dc6b23637a9f2105c75
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-02-14 03:55:08 +0900

    [Gold IV] Title: 연구소, Time: 1480 ms, Memory: 36040 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,34 +1,34 @@
-from collections import deque
 from itertools import combinations
+from collections import deque
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
 n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-result = 0
-
+graph = [list(map(int,input().split())) for _ in range(n)]
 empty_space = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 0]
 virus_space = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 2]
+answer = 0
 
 def bfs(graph):
     queue = deque(virus_space)
     while queue:
-        cur_x, cur_y = queue.popleft()
+        x, y = queue.popleft()
         for i in range(4):
-            next_x = cur_x + dx[i]
-            next_y = cur_y + dy[i]
-            if 0 <= next_x < n and 0 <= next_y < m and graph[next_x][next_y] ==  0:
-                graph[next_x][next_y] = 2
-                queue.append((next_x, next_y))
-    
-    return sum([row.count(0) for row in graph])
+            next_x = x + dx[i]
+            next_y = y + dy[i]
+            if 0 <= next_x < n and 0 <= next_y < m and graph[next_x][next_y] == 0:
+                    graph[next_x][next_y] = 2
+                    queue.append((next_x, next_y))
+
+    return sum(row.count(0) for row in graph)
 
 for walls in combinations(empty_space, 3):
-    copy_graph = [row[::] for row in graph]
+    new_graph = [row[:] for row in graph]
     for x, y in walls:
-        copy_graph[x][y] = 1
-    temp = bfs(copy_graph)
-    result = max(temp, result)
+        new_graph[x][y] = 1
 
-print(result)
+    temp = bfs(new_graph)
+    answer = max(answer, temp)
+
+print(answer)
