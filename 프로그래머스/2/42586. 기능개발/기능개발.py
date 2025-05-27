ecbc131f8bb5979d37627d5a3258e380c05c5632
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-05-28 01:24:46 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -4,12 +4,12 @@ def solution(progresses, speeds):
     answer = []
     time = 0
     count = 0
-    
+
     progresses = deque(progresses)
     speeds = deque(speeds)
-    
+
     while progresses:
-        if progresses[0] + time * speeds[0] >= 100:
+        if progresses[0] + (time * speeds[0]) >= 100:
             progresses.popleft()
             speeds.popleft()
             count += 1  # 완료된 작업 개수 증가
@@ -18,9 +18,9 @@ def solution(progresses, speeds):
                 answer.append(count)  # 완료된 작업 수 기록
                 count = 0
             time += 1  # 하루 경과
-    
+
     # 마지막으로 남은 작업 개수 추가
     if count > 0:
         answer.append(count)
-    
+
     return answer
