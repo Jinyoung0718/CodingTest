338f2016151a7c7279d655aa1343db97c2f8bffc
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-05-28 00:14:06 +0900

    [level 3] Title: 베스트앨범, Time: 0.11 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,16 +1,29 @@
 def solution(genres, plays):
     answer = []
-    genre_songs = {}
-    genre_play_count = {}
-    
-    for index, (genre, play) in enumerate(zip(genres, plays)):
-        genre_songs.setdefault(genre, []).append((index, play))
-        genre_play_count[genre] = genre_play_count.get(genre, 0) + play
-    
-    sorted_genres = sorted(genre_play_count.items(), key=lambda x : x[1], reverse = True)
-    for genre, total_play in sorted_genres:
-        top_songs = sorted(genre_songs[genre], key=lambda x: x[1], reverse = True)[:2]
-        for index, play in top_songs:
-            answer.append(index)
-    
+    genre_to_songs = {}       # 장르별로 (고유번호, 재생 수) 튜플 리스트
+    genre_total_plays = {}    # 장르별 총 재생 수
+
+    for song_id, (genre, play_count) in enumerate(zip(genres, plays)):
+        # 장르별 노래 모음
+        if genre not in genre_to_songs:
+            genre_to_songs[genre] = [(song_id, play_count)]
+        else:
+            genre_to_songs[genre].append((song_id, play_count))
+
+        # 장르별 총 재생 수 계산
+        if genre not in genre_total_plays:
+            genre_total_plays[genre] = play_count
+        else:
+            genre_total_plays[genre] += play_count
+
+    # 장르를 총 재생 수 기준으로 정렬
+    sorted_genres = sorted(genre_total_plays.items(), key=lambda x: x[1], reverse=True)
+
+    for genre, _ in sorted_genres:
+        # 해당 장르의 노래들을 재생 수 내림차순, 고유번호 오름차순으로 정렬
+        sorted_songs = sorted(genre_to_songs[genre], key=lambda x: (-x[1], x[0]))
+        top_two_songs = sorted_songs[:2]  # 최대 2곡까지 수록
+        for song_id, _ in top_two_songs:
+            answer.append(song_id)
+
     return answer
