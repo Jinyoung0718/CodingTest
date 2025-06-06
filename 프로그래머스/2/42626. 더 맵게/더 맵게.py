eff07fa04445e75e0b1c5d918a452a4f5e565360
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-07 00:37:42 +0900

    [level 2] Title: 더 맵게, Time: 2002.81 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,15 +1,17 @@
 import heapq
 
 def solution(scoville, K):
-    heapq.heapify(scoville)  # 최소 힙 변환
     answer = 0
+    heapq.heapify(scoville)
+
+    while len(scoville) >= 2 and scoville[0] < K:
+        i = heapq.heappop(scoville)
+        j = heapq.heappop(scoville)
+        new = i + (j * 2)
+        heapq.heappush(scoville, new)
+        answer += 1
+
+    if scoville[0] < K:
+        return -1
     
-    while len(scoville) > 1 and scoville[0] < K:
-        first = heapq.heappop(scoville)  # 가장 작은 값
-        second = heapq.heappop(scoville)  # 두 번째 작은 값
-        new_scoville = first + (second * 2)  # 새로운 음식의 스코빌 지수
-        heapq.heappush(scoville, new_scoville)  # 힙에 추가
-        answer += 1  # 섞은 횟수 증가
-    
-    # 모든 음식의 스코빌 지수가 K 이상인지 확인
-    return answer if scoville[0] >= K else -1
+    return answer
