d56ebfef85256c93ce696575144a720ff1a221e5
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-10 14:50:41 +0900

    [level 2] Title: H-Index, Time: 0.20 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -2,6 +2,6 @@ def solution(citations):
     citations.sort(reverse=True)
     
     for i, c in enumerate(citations):
-        if i >= c:
+        if i + 1 >= c:
             return i
     return len(citations)
