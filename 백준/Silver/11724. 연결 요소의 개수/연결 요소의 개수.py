571b49ef448903aabef9a935d6089c9b2d012987
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-11 00:20:15 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 288 ms, Memory: 172968 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,33 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
-n ,m = map(int ,input().split())
-graph = [[] * (n + 1) for _ in range(n + 1)]
+n, m = map(int, input().split())
+tree = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
-count = 0
+answer = 0
 
 for _ in range(m):
-    s, e = map(int ,input().split())
-    graph[s].append(e)
-    graph[e].append(s)
+    start, end = map(int, input().split())
+    tree[start].append(end)
+    tree[end].append(start)
 
+def dfs(num):
+    visited[num] = True
+    for cur_num in tree[num]:
+        if not visited[cur_num]:
+            dfs(cur_num)
 
-def bfs(cur_v):
-    visited[cur_v] = True 
-    queue = deque()
-    queue.append(cur_v)
-    while queue:
-        cur_v = queue.popleft()
-        for next_v in graph[cur_v]:
-            if not visited[next_v]:
-                visited[next_v] = True
-                queue.append(next_v) 
-
-for i in range(1, n+1):
+for i in range(1, n + 1):
     if not visited[i]:
-        bfs(i)
-        count += 1
+        dfs(i)
+        answer += 1
 
-print(count)
+print(answer)
