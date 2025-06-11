821224020d9767838f7724549ee697e645678430
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-11 13:36:56 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,13 @@
 def solution(clothes):
+    
     memo = {}
-
-    for cloth, type in clothes:
-        memo[type] = memo.get(type, 0) + 1  # ✅ 정확한 카운팅
-
     answer = 1
-    for count in memo.values():
-        answer *= (count + 1)
-
-    return answer - 1  # 공집합 제거
+    
+    for cloth, kind in clothes:
+        memo[kind] = memo.get(kind, 0) + 1
+    
+    
+    for val in memo.values():
+        answer *= (val + 1)
+    
+    return answer - 1
