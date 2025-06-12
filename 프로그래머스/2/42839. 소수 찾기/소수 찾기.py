046e91970476c9af6a6f69822ef239165299616c
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-12 18:53:35 +0900

    [level 2] Title: 소수 찾기, Time: 5.60 ms, Memory: 9.55 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,27 @@
 from itertools import permutations
 
-def sieve(n):
-    is_prime = [False, False] + [True] * (n - 1)
-    for i in range(2, int(n ** 0.5) + 1):
-        if is_prime[i]:
-            for j in range(i*i, n + 1, i):
-                is_prime[j] = False
-    return is_prime
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    
+    return True
 
 def solution(numbers):
     number_set = set()
-
+    
     for i in range(1, len(numbers) + 1):
         for perm in permutations(numbers, i):
             num = int(''.join(perm))
             number_set.add(num)
-
-    max_val = max(number_set)
-    is_prime = sieve(max_val)
-
-    return sum(1 for num in number_set if is_prime[num])
-
+    
+    count = 0
+    
+    for num in number_set:
+        if is_prime(num):
+            count += 1
+    
+    return count
