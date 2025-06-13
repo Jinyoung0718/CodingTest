6ecd084290f685675117c048140a5d10d8c11587
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-13 17:15:06 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 236 ms, Memory: 67000 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,29 +1,24 @@
-from collections import deque
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
 n = int(input())
-tree = [[] for _ in range (n + 1)]
-parent = [0] * (n + 1)
+tree = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
+answer = [0] * (n + 1)
 
+for _ in range(n - 1):
+    start, end = map(int, input().split())
+    tree[start].append(end)
+    tree[end].append(start)
 
-for _ in range(n-1):
-    s, e = map(int, input().split())
-    tree[s].append(e)
-    tree[e].append(s)
+def dfs(num):
+    visited[num] = True
+    for next_num in tree[num]:
+        if not visited[next_num]:
+            answer[next_num] = num
+            dfs(next_num)
 
-def levelorder(node):
-    visited[node] = True
-    queue = deque()
-    queue.append(node)
-    while queue:
-        cur_node = queue.popleft()
-        for next_node in tree[cur_node]:
-            if not visited[next_node]:
-                visited[next_node] = True
-                parent[next_node] = cur_node
-                queue.append(next_node)
+dfs(1)
 
-levelorder(1)
-
-for i in range(2,n+1):
-    print(parent[i])
+print("\n".join(map(str, answer[2:])))
