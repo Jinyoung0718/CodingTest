3539c9ebe487614c558d82c12d9f42af28b3960b
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-15 01:12:15 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,16 +1,9 @@
-memo = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-word = input()
-i = 0
-answer = 0
+croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
 
-while i < len(word):
-    for m in memo:
-        if word[i:i+len(m)] == m:
-            answer += 1
-            i += len(m)
-            break
-    else:
-        answer += 1
-        i += 1
+sentence = input()
+idx = 0
 
-print(answer)
+for word in croatia:
+    sentence = sentence.replace(word, "*")
+
+print(len(sentence))
