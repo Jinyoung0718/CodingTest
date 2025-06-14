9bf39452a71676889111810ad06cc7ab0cd349bd
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-14 19:20:31 +0900

    [Silver II] Title: 사탕 게임, Time: 1112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,43 +1,41 @@
-def change(data):
+n = int(input())
+graph = [list(input()) for _ in range(n)]
+
+dx = [0, 1]
+dy = [1, 0]
+
+def check(graph):
     max_count = 1
+    for i in range(n):
+        count = 1
+        for j in range(1, n):
+            if graph[i][j] == graph[i][j-1]:
+                count += 1
+                max_count = max(max_count, count)
+            else:
+                count = 1
 
     for i in range(n):
+        count = 1
+        for j in range(1, n):
+            if graph[j][i] == graph[j-1][i]:
+                count += 1
+                max_count = max(max_count, count)
+            else:
+                count = 1
 
-        count = 1
-        for j in range(1, n):
-            if data[i][j] == data[i][j - 1]:
-                count += 1
-            else:
-                count = 1
-            max_count = max(max_count, count)
-    
-        count = 1
-        for j in range(1, n):
-            if data[j][i] == data[j - 1][i]:
-                count += 1
-            else:
-                count = 1
-            max_count = max(max_count, count)
-    
     return max_count
 
-
-n = int(input())
-data = [list(input()) for _ in range(n)]
-result = 0
+answer = 0
 
 for i in range(n):
     for j in range(n):
-        if j + 1 < n:
-            data[i][j], data[i][j + 1] = data[i][j + 1], data[i][j]
-            cnt = change(data)
-            result = max(result, cnt)
-            data[i][j], data[i][j + 1]  = data[i][j + 1], data[i][j]
-    
-        if i + 1 < n:
-            data[i][j], data[i + 1][j] = data[i + 1][j], data[i][j]
-            cnt = change(data)
-            result = max(result, cnt)
-            data[i][j], data[i + 1][j] = data[i + 1][j], data[i][j]
+        for k in range(2):
+            ni = i + dx[k]
+            nj = j + dy[k]
+            if 0 <= ni < n and 0 <= nj < n and graph[i][j] != graph[ni][nj]:
+                graph[i][j], graph[ni][nj] = graph[ni][nj], graph[i][j]
+                answer = max(answer, check(graph))
+                graph[i][j], graph[ni][nj] = graph[ni][nj], graph[i][j]
 
-print(result)
+print(answer)
