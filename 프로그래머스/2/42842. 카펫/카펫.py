b75444ce52454716afe273d560e6e26c8e8182e8
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-15 22:34:52 +0900

    [level 2] Title: 카펫, Time: 42.94 ms, Memory: 47.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,18 +1,22 @@
 def solution(brown, yellow):
     answer = []
-
-    sum = brown + yellow
-
-    for i in range(3,sum):
-        col = i
-        row = sum/i
-        if sum%i == 0 :
+    
+    total = brown + yellow
+    
+    for num in range(3, total):
+        
+        col = num
+        row = total // num
+        
+        if total % num == 0:
             b = col + col + row + row - 4
-            y = sum - b
-            print(col,row,b,y)
-            if brown == b and yellow == y :
+            y = total - b
+            
+            if b == brown and y == yellow:
                 answer.append(row)
                 answer.append(col)
                 break
-
+    
     return answer
+        
+        
