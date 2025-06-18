b351fad73c76860cfbf6cb00f6b18f05cf4a3b5e
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-18 16:45:46 +0900

    [Silver I] Title: 영역 구하기, Time: 64 ms, Memory: 35000 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,40 +1,43 @@
 from collections import deque
 
-m, n, k = map(int, input().split()) 
-graph = [[0] * n for _ in range(m)] 
+M, N, K = map(int, input().split())  # M: 세로, N: 가로
+graph = [[0] * N for _ in range(M)]
+visited = [[False] * N for _ in range(M)]
+answer = []
 
-for _ in range(k):
-    lx, ly, rx, ry = map(int, input().split())  
-    for i in range(ly, ry):
-        for j in range(lx, rx):
-            graph[m - i - 1][j] = -1  
+dx = [-1, 0, 1, 0]  # 상, 좌, 하, 우
+dy = [0, -1, 0, 1]
 
-directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-def bfs(start_x, start_y):
+def bfs(x, y):
     queue = deque()
-    queue.append((start_x, start_y))
-    graph[start_x][start_y] = 1 
-    area = 1
+    queue.append((x, y))
+    visited[x][y] = True
+    count = 1
 
     while queue:
-        x, y = queue.popleft()
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < m and 0 <= ny < n:
-                if graph[nx][ny] == 0:
-                    graph[nx][ny] = 1 
+        cur_x, cur_y = queue.popleft()
+        for i in range(4):
+            nx = cur_x + dx[i]
+            ny = cur_y + dy[i]
+            if 0 <= nx < M and 0 <= ny < N:
+                if not visited[nx][ny] and graph[nx][ny] == 0:
+                    visited[nx][ny] = True
                     queue.append((nx, ny))
-                    area += 1
+                    count += 1
+    return count
 
-    return area
+for _ in range(K):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            graph[i][j] = 1
 
-areas = []
-for i in range(m):
-    for j in range(n):
-        if graph[i][j] == 0:  
-            areas.append(bfs(i, j))
+for i in range(M):
+    for j in range(N):
+        if graph[i][j] == 0 and not visited[i][j]:
+            area = bfs(i, j)
+            answer.append(area)
 
-areas.sort()
-print(len(areas))
-print(*areas)
+answer.sort()
+print(len(answer))
+print(' '.join(map(str, answer)))
