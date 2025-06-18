ae2842801279c489f654d9869a80954f67a0af2c
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-18 12:58:15 +0900

    [level 2] Title: 피로도, Time: 28.69 ms, Memory: 9.48 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -2,15 +2,18 @@ from itertools import permutations
 
 def solution(k, dungeons):
     answer = []
-
+    
     for perm in permutations(dungeons):
         total = 0
         k_copy = k
         for i, j in perm:
-            if i > k_copy:
+            
+            if k_copy < i:
                 break
+            
             k_copy -= j
             total += 1
+        
         answer.append(total)
-
+    
     return max(answer)
