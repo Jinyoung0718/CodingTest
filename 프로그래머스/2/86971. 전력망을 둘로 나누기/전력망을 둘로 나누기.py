7f2f958de4b3cde5ff5ee61ef79374ea4157fd7e
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-21 21:29:13 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.78 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,39 +1,43 @@
 from collections import deque
 
-def bfs(graph, start, visited):
-    queue = deque([start])
-    visited[start] = True
-    count = 0
-
+def bfs(graph, visited, start_node):
+    visited[start_node] = True
+    queue = deque()
+    queue.append((start_node))
+    count = 1
+    
     while queue:
-        node = queue.popleft()
-        count += 1
-        for neighbor in graph[node]:
-            if not visited[neighbor]:
-                visited[neighbor] = True
-                queue.append(neighbor)
-
+        cur_node = queue.popleft()
+        for next_node in graph[cur_node]:
+            if not visited[next_node]:
+                visited[next_node] = True
+                queue.append(next_node)
+                count += 1
+    
     return count
-
+            
+        
 def solution(n, wires):
     answer = n - 2
-
+    
     for i in range(len(wires)):
-        temp = wires[:i] + wires[i+1:] # i번째 전선을 끊는 역할
+        temp = wires[:i] + wires[i+1:]
         graph = [[] for _ in range(n + 1)]
         visited = [False] * (n + 1)
-
+        
         for x, y in temp:
             graph[x].append(y)
             graph[y].append(x)
-
+        
         for node in range(1, n + 1):
             if graph[node]:
                 start = node
                 break
-
-        a = bfs(graph, start, visited)
+        
+        a = bfs(graph, visited, start)
         b = n - a
         answer = min(answer, abs(a - b))
-
+    
     return answer
+    
+    
