f64280d6c2be2868214f43c26835f3cf42cb49e5
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-25 19:35:05 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,28 +1,27 @@
 def solution(genres, plays):
-    answer = []
     
-    genre_to_songs = {}
-    genre_to_total = {}
+    answer = []
+    genre_total = {}
+    genre_plays = {}
     
     for idx, (genre, play) in enumerate(zip(genres, plays)):
         
-        if genre not in genre_to_songs:
-            genre_to_songs[genre] = [(idx, play)]
+        if genre not in genre_plays:
+            genre_plays[genre] = [(idx, play)]
         else:
-            genre_to_songs[genre].append((idx, play))
+            genre_plays[genre].append((idx, play))
         
-        if genre not in genre_to_total:
-            genre_to_total[genre] = play
+        
+        if genre not in genre_total:
+            genre_total[genre] = play
         else:
-            genre_to_total[genre] += play
+            genre_total[genre] += play
     
-    sorted_genre_to_total = sorted(genre_to_total.items(), key=lambda x : x[1], reverse = True)
+    sort_genre_total = sorted(genre_total.items(), key = lambda x : x[1], reverse = True)
     
-    for genre, _ in sorted_genre_to_total:
-        sorted_genre_to_songs = sorted(genre_to_songs[genre], key=lambda x : (-x[1], x[0]))
-        top_two_songs = sorted_genre_to_songs[:2]
-        
-        for idx, _ in top_two_songs:
+    for key, _ in sort_genre_total:
+        sort_genre_plays = sorted(genre_plays[key], key = lambda x : (-x[1], x[0]))
+        for idx, _ in sort_genre_plays[:2]:
             answer.append(idx)
     
     return answer
