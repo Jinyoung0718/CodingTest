e0d3194c90238bc9596e2c9b91c95ce20f3d56d7
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-26 17:16:49 +0900

    [level 3] Title: 이중우선순위큐, Time: 95.36 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,21 +1,42 @@
 import heapq
 
 def solution(operations):
-    heap = []
+    min_heap = []
+    max_heap = []
+    visited = [False] * len(operations)  # 각 원소의 유효성 추적
 
-    for operation in operations:
-        operator, operand = operation.split(' ')
-        operand = int(operand)
+    for i, operation in enumerate(operations):
+        op, num = operation.split()
+        num = int(num)
 
-        if operator == 'I':
-            heapq.heappush(heap, operand)
-        elif heap:
-            if operand < 0:
-                heapq.heappop(heap)
-            else:
-                heap.remove(max(heap))
+        if op == 'I':
+            heapq.heappush(min_heap, (num, i))
+            heapq.heappush(max_heap, (-num, i))
+            visited[i] = True
+        else:
+            if num == 1:  # 최댓값 삭제
+                while max_heap and not visited[max_heap[0][1]]:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    visited[max_heap[0][1]] = False
+                    heapq.heappop(max_heap)
+                    
+            else:         # 최솟값 삭제
+                while min_heap and not visited[min_heap[0][1]]:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    visited[min_heap[0][1]] = False
+                    heapq.heappop(min_heap)
 
-    if not heap:
+                    
+    # 최종 정리: 아직 살아있는 값만 남김
+    while min_heap and not visited[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+        
+    while max_heap and not visited[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+
+    if not min_heap or not max_heap:
         return [0, 0]
-
-    return [max(heap), heap[0]]
+    
+    return [-max_heap[0][0], min_heap[0][0]]
