416a0958cbc215d43ae9eefe77a64cf599aa5136
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-26 17:53:11 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,20 +1,24 @@
 def solution(name):
-    if set(name) == {'A'}:
-        return 0
-    
-    a_pos = ord('A') # 'A' : 65, 'Z' : 90
-    z_pos = ord('Z')
-    
-    answer = float('inf')
-    
-    for i in range(len(name)//2 + 1):
-        l_r = name[-i:] + name[:-i] #왼쪽먼저 갔다가 + 오른쪽
-        r_l = name[i: :-1] + name[i+1:][::-1] # 기준점에서 빠꾸 + 좌측
-        for n in [l_r,r_l]:
-            # 끝에 A들은 셀 필요 없으므로 자르기
-            while n and n[-1] == 'A':
-                n = n[:-1]
-            cnt = [min(ord(c)-a_pos, (z_pos+1) - ord(c)) for c in n ]
-            answer = min(answer, i + (len(cnt)-1) + sum(cnt))
+    answer = 0
+    n = len(name)
 
-    return answer
+    # 알파벳 변경 비용
+    for char in name:
+        answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+
+    # 커서 이동 최소 계산
+    move = n - 1
+    for i in range(n):
+        next_i = i + 1
+        # 연속된 A의 끝까지 찾기
+        while next_i < n and name[next_i] == 'A':
+            next_i += 1
+        # i에서 연속된 A를 건너뛰는 3가지 방향 비교
+        distance = min(
+            move,                         # 그냥 오른쪽 쭉 가기
+            i * 2 + n - next_i,           # 왼쪽 갔다가 오른쪽 가기
+            (n - next_i) * 2 + i          # 오른쪽 갔다가 왼쪽 가기
+        )
+        move = min(move, distance)
+
+    return answer + move
