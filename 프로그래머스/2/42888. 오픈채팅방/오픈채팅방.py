83e81cfe30dee60b9251bad368fcc25a0b7eeb8f
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-30 20:16:07 +0900

    [level 2] Title: 오픈채팅방, Time: 115.60 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,24 +1,26 @@
 def solution(records):
     answer = []
     memo = {}
-
+    
     for record in records:
         parts = record.split()
         command = parts[0]
         user_id = parts[1]
-
-        if command == 'Enter' or command == 'Change':
-            nickname = parts[2]
-            memo[user_id] = nickname
-
+        
+        if command == "Enter" or command == "Change":
+            nick_name = parts[2]
+            memo[user_id] = nick_name
+    
+        
     for record in records:
         parts = record.split()
         command = parts[0]
         user_id = parts[1]
-
-        if command == 'Enter':
+        
+        if command == "Enter":
             answer.append(f"{memo[user_id]}님이 들어왔습니다.")
-        elif command == 'Leave':
+        
+        if command == "Leave":
             answer.append(f"{memo[user_id]}님이 나갔습니다.")
-
+    
     return answer
