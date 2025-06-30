811d1240788373fabf8fbf5aa68589708f2aac0a
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-06-30 20:47:11 +0900

    [level 3] Title: 가장 먼 노드, Time: 27.97 ms, Memory: 20 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def solution(n, edge):
+    
+    graph = [[] for _ in range(n + 1)]
+
+    for start, end in edge:
+        graph[start].append(end)
+        graph[end].append(start)
+    
+    visited = [False] * (n + 1)
+    distance = [0] * (n + 1)
+    answer = 0
+    
+    
+    def bfs(start_node):
+        visited[start_node] = True
+        queue = deque()
+        queue.append((start_node, 0))
+        while queue:
+            cur_node, cur_val = queue.popleft()
+            distance[cur_node] = cur_val
+            
+            for next_node in graph[cur_node]:
+                if not visited[next_node]:
+                    visited[next_node] = True
+                    queue.append((next_node, cur_val + 1))
+        
+    bfs(1)
+    max_dist = max(distance)
+    return distance.count(max_dist)
