559db2ef2914fc3fb9a4a6764a148342f118cd2b
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-07-04 19:48:13 +0900

    [level 3] Title: 단어 변환, Time: 0.18 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+def can_convert(word1, word2):
+    diff = 0
+    
+    for a, b in zip(word1, word2):
+        if a != b:
+            diff += 1        
+            if diff > 1:
+                return False
+    
+    return diff == 1
+            
+
+def solution(begin, target, words):
+    
+    if target not in words:
+        return 0
+    
+    queue = deque()
+    queue.append((begin, 0))
+    visited = set()
+    
+    while queue:
+        cur_word, depth = queue.popleft()
+        
+        if cur_word == target:
+            return depth
+        
+        for word in words:
+            if word not in visited and can_convert(cur_word, word):
+                visited.add(word)
+                queue.append((word, depth + 1))
