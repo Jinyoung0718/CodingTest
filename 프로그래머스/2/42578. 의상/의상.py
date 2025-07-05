39dce505701ac966afb977bae26c93a38d4d3b47
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-07-05 15:15:44 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -3,10 +3,10 @@ def solution(clothes):
     memo = {}
     answer = 1
     
-    for cloth, kind in clothes:
+    for _, kind in clothes:
         memo[kind] = memo.get(kind, 0) + 1
     
-    for key in memo.keys():
-        answer *= (memo[key] + 1)
+    for value in memo.values():
+        answer *= (value + 1)
     
     return answer - 1
