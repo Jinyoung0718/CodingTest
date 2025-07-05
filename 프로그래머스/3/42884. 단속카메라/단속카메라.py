ea0ca3d432a7bfbc8294afba00375abc52786862
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-07-05 16:32:17 +0900

    [level 3] Title: 단속카메라, Time: 2.39 ms, Memory: 9.86 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -0,0 +1,13 @@
+def solution(routes):
+    routes.sort(key=lambda x: x[1])  # 진출 시점 기준 정렬
+    print(routes)
+    
+    camera_count = 0
+    last_camera_position = -30001  # 카메라 설치 안 된 초기값
+
+    for route in routes:
+        if route[0] > last_camera_position:
+            camera_count += 1
+            last_camera_position = route[1]
+
+    return camera_count
