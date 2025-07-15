c49d645f5e4f9540fb81c7cd3446e1d240ba6914
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-07-15 15:33:02 +0900

    [Gold V] Title: 동전 2, Time: 404 ms, Memory: 43540 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,22 +1,31 @@
 n, k = map(int, input().split())
+coins = []
 
-coins = set(int(input()) for _ in range(n))
+for _ in range(n):
+    coins.append(int(input()))
 
 INF = k + 1
-dp = [[INF] * (k + 1) for _ in range(len(coins) + 1)]
+dp = [[INF] * (k + 1) for _ in range(n + 1)]
+coins = list(set(coins))
+n = len(coins) # coin의 중복제거로 n이 줄어듦을 고려
 
-coins = list(coins)
-
-for i in range(len(coins) + 1):
+for i in range(n + 1):
     dp[i][0] = 0
 
-for i in range(1, len(coins) + 1):
+for i in range(1, n + 1):
     coin = coins[i - 1]
     for j in range(1, k + 1):
         if j >= coin:
-            dp[i][j] = min(dp[i - 1][j], dp[i][j - coin] + 1)
+            dp[i][j] = min(
+                dp[i - 1][j],
+                dp[i][j - coin] + 1
+            )
         else:
             dp[i][j] = dp[i - 1][j]
 
-ans = dp[len(coins)][k]
-print(-1 if ans == INF else ans)
+answer = dp[n][k]
+
+if answer == INF:
+    print(-1)
+else:
+    print(answer)
