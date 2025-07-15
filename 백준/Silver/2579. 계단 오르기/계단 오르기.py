2b72f0d3e8862ce220fa9f1def444bc57827da4b
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-07-15 16:27:46 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = [0] + [int(input()) for _ in range(n)]  # 1-based index
+
+dp = [0] * (n + 1)
+
+if n == 1:
+    dp[1] = arr[1]
+elif n == 2:
+    dp[1] = arr[1]
+    dp[2] = arr[1] + arr[2]
+else:
+    dp[1] = arr[1]
+    dp[2] = arr[1] + arr[2]
+    dp[3] = max(arr[1] + arr[3], arr[2] + arr[3])
+    
+    for i in range(4, n + 1):
+        dp[i] = max(
+            dp[i - 2] + arr[i],
+            dp[i - 3] + arr[i - 1] + arr[i]
+        )
+
+print(dp[n])
