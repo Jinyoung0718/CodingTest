7fee3f7564213c6379bc4de63a8b44f5dfd58572
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-07-22 14:09:01 +0900

    [level 2] Title: 피로도, Time: 26.00 ms, Memory: 9.61 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -4,16 +4,17 @@ def solution(k, dungeons):
     answer = []
     
     for perm in permutations(dungeons):
-        total = 0
-        k_copy = k
-        for i, j in perm:
+        cleared = 0
+        cur_fatigue = k # 현재 피로도 -> 순회해야 함으로 복사
+        
+        for required, consumed in perm:
             
-            if k_copy < i:
+            if cur_fatigue < required:
                 break
             
-            k_copy -= j
-            total += 1
+            cur_fatigue -= consumed
+            cleared += 1
         
-        answer.append(total)
+        answer.append(cleared)
     
     return max(answer)
