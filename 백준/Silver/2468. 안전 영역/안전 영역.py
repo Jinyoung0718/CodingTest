fdfabb92cd7e91769b117e450546714f11fdc2d7
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-07-22 13:54:30 +0900

    [Silver I] Title: 안전 영역, Time: 688 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -2,33 +2,36 @@ from collections import deque
 
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+max_rain = max(map(max, graph))
 answer = 0
 
-def bfs(x, y, rain):
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+def bfs(x, y, visited, rain):
+    visited[x][y] = True
     queue = deque()
     queue.append((x, y))
-    visited[x][y] = True
     while queue:
         cur_x, cur_y = queue.popleft()
+
         for i in range(4):
             next_x = cur_x + dx[i]
             next_y = cur_y + dy[i]
             if 0 <= next_x < n and 0 <= next_y < n:
-                if graph[next_x][next_y] > rain and not visited[next_x][next_y]:
+                if not visited[next_x][next_y] and graph[next_x][next_y] > rain:
                     visited[next_x][next_y] = True
                     queue.append((next_x, next_y))
 
-for rain in range(0, max(map(max, graph)) + 1):
+for rain in range(0, max_rain + 1):
+    safe_area = 0
     visited = [[False] * n for _ in range(n)]
-    count = 0
     for i in range(n):
         for j in range(n):
-            if graph[i][j] > rain and not visited[i][j]:
-                bfs(i, j, rain)
-                count += 1
+            if not visited[i][j] and graph[i][j] > rain:
+                bfs(i, j, visited, rain)
+                safe_area += 1
 
-    answer = max(answer, count)
+    answer = max(answer, safe_area)
 
 print(answer)
