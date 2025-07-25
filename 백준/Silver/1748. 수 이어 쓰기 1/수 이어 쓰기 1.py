aa2cd599bee2175abee2a4197d42694981da7736
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-07-25 14:36:37 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -6,7 +6,6 @@ answer = 0
 
 for i in range(comp):
     answer += 9 * (10 ** i) * (i + 1)
-    i += 1
-answer += ((int(n) - (10 ** comp)) + 1) * (comp + 1)
 
+answer += ((int(n) - (10 ** comp)) + 1) * (comp + 1)
 print(answer)
