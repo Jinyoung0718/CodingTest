f8368cae5dc1a89435ec445fd35ba40ddef58035
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-08-13 15:31:22 +0900

    [level 3] Title: 여행경로, Time: 0.05 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,3 +1,18 @@
+import heapq
+from collections import defaultdict
+
 def solution(tickets):
-    answer = []
-    return answer
+    graph = defaultdict(list)
+    route = []
+    
+    for a, b in tickets:
+        heapq.heappush(graph[a], b) # 목적지를 사전순(min-heap)으로 관리
+    
+    def dfs(start_airport):
+        while graph[start_airport]:
+            dfs(heapq.heappop(graph[start_airport])) # 사전순으로 가장 작은 목적지부터 소모
+            
+        route.append(start_airport) # 더 갈 곳이 없을 때 경로에 추가 (역순 쌓임 -> 목적지 부터 쌓임)
+
+    dfs("ICN")
+    return route[::-1]  # 역순 반환
