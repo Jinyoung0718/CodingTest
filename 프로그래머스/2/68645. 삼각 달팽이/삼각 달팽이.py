f0c1c230cf8ecc03e75cae5f5fbd8e32fa8f42da
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-08-22 16:50:08 +0900

    [level 2] Title: 삼각 달팽이, Time: 172.06 ms, Memory: 42.2 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.py
@@ -4,22 +4,25 @@ def solution(n):
     num = 1
     x, y = -1, 0
     
+    # 현재 이동 방향이 몇 번째 단계인지를 나타냄
     for i in range(n):
+        # 안쪽 for문이 방향 하나를 ‘쭉’ 끝까지 처리
+        # 한 방향으로 움직이는 칸 수가 매 턴마다 1씩 줄어듦
         for _ in range(i, n):
             
-            if i % 3 == 0:      # 아래
+            if i % 3 == 0:          # 아래
                 x += 1
             
-            elif i % 3 == 1:    # 오른쪽
+            elif i % 3 == 1:        # 오른쪽
                 y += 1
             
-            else:               # 왼쪽 위
+            elif i % 3 == 2:                   # 왼쪽 위
                 x -= 1
                 y -= 1
-            
+        
             tri[x][y] = num
             num += 1
-            
+    
     for row in tri:
         for num in row:
             answer.append(num)
