f65d61b52210bedfd0c53c71bc0cfbf09d3d5e65
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-08-22 17:31:33 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.42 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,28 +1,28 @@
-import re
-
 def solution(new_id):
-    # 1단계: 소문자로 변환
+    answer = ''
+    # 1
     new_id = new_id.lower()
-    
-    # 2단계: 허용 문자만 남기기 (a-z, 0-9, -, _, .)
-    new_id = re.sub(r'[^a-z0-9\-_.]', '', new_id)
-    
-    # 3단계: 마침표 2번 이상 → 1개로
-    new_id = re.sub(r'\.+', '.', new_id)
-    
-    # 4단계: 처음과 끝의 마침표 제거
-    new_id = new_id.strip('.')
-    
-    # 5단계: 빈 문자열이면 'a' 대입
-    if new_id == '':
-        new_id = 'a'
-    
-    # 6단계: 길이 16 이상이면 15자까지만 남김, 끝 마침표 제거
-    if len(new_id) >= 16:
-        new_id = new_id[:15].rstrip('.')
-    
-    # 7단계: 길이 2 이하라면 마지막 문자를 3이 될 때까지 반복
-    while len(new_id) < 3:
-        new_id += new_id[-1]
-    
-    return new_id
+    # 2
+    for c in new_id:
+        if c.isalpha() or c.isdigit() or c in ['-', '_', '.']:
+            answer += c
+    # 3
+    while '..' in answer:
+        answer = answer.replace('..', '.')
+    # 4
+    if answer[0] == '.':
+        answer = answer[1:] if len(answer) > 1 else '.'
+    if answer[-1] == '.':
+        answer = answer[:-1]
+    # 5
+    if answer == '':
+        answer = 'a'
+    # 6
+    if len(answer) > 15:
+        answer = answer[:15]
+        if answer[-1] == '.':
+            answer = answer[:-1]
+    # 7
+    while len(answer) < 3:
+        answer += answer[-1]
+    return answer
