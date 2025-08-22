d3c88c0a189230bf957e394da7f5d77621557a00
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-08-22 17:28:33 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.39 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -2,21 +2,27 @@ def solution(survey, choices):
     answer = ""
     
     pairs = [('R','T'), ('C','F'), ('J','M'), ('A','N')]
-    score = { 'R':0, 'T':0, 'C':0, 'F':0, 'J':0, 'M':0, 'A':0, 'N':0 }
+    score = {'R':0, 'T':0, 'C':0, 'F':0, 'J':0, 'M':0, 'A':0, 'N':0}
     
     for s, c in zip(survey, choices):
         disagree, agree = s[0], s[1]
-        if c < 4:  # 비동의 쪽
+        
+        if c in [1, 2, 3]:  # 비동의 쪽
             score[disagree] += 4 - c
-        elif c > 4:  # 동의 쪽
+            
+        if c in [5, 6, 7]:  # 동의 쪽
             score[agree] += c - 4
     
     for a, b in pairs:
+        # 성격은 각 지표에서 두 유형 중 하나로 결정됩니다
         if score[a] > score[b]:
             answer += a
+            
         elif score[a] < score[b]:
             answer += b
+        
         else:
-            answer += min(a, b)  # 동점이면 사전순 빠른 쪽
+            answer += min(a, b)  # 동점이면 사전순 빠른 쪽 -> min으로 알파벳 사전순 추출 가능
+    
     
     return answer
