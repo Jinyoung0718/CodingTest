0200c67bef8e7a73dfc21a5df6c042da3beef95f
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-08-24 01:48:55 +0900

    [level 1] Title: 실패율, Time: 12.90 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,21 +1,21 @@
 def solution(N, stages):
     answer = []
-    
+    players = len(stages)
     count = [0] * (N + 2)
+    
+    # 사용자들이 현재 머물고 있고 클리어하지 못한 스테이지
     for stage in stages:
         count[stage] += 1
-
-    total = len(stages)
     
     for stage in range(1, N + 1):
-        if total == 0:
-            fail = 0
-            
+        
+        if players == 0:
+            fail = 0    
         else:
-            fail = count[stage] / total
+            fail = count[stage] / players
         
         answer.append((fail, stage))
-        total -= count[stage]  # 다음 스테이지로 넘어갈 '도달 인원' 갱신
+        players -= count[stage]  # 다음 스테이지로 넘어갈 '도달 인원' 갱신
 
     answer.sort(key=lambda x: (-x[0], x[1]))
     
