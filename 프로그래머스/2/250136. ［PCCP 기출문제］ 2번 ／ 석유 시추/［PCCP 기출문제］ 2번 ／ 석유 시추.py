41ef6677189532056d164af4a25820bf22bdd152
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-08-24 15:15:16 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 347.04 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -4,7 +4,7 @@ def solution(land):
     n, m = len(land), len(land[0])
     visited = [[False] * m for _ in range(n)]
     col_sum = [0] * m
-
+    
     dx = [-1, 0, 1, 0]
     dy = [0, -1, 0, 1]
     
@@ -16,23 +16,21 @@ def solution(land):
                 visited[i][j] = True
                 size = 0
                 cols = set()
-
+                
                 while queue:
-                    x, y = queue.popleft()
+                    cur_x, cur_y = queue.popleft()
                     size += 1
-                    cols.add(y)
-
+                    cols.add(cur_y)
+                    
                     for d in range(4):
-                        nx = x + dx[d]
-                        ny = y + dy[d]
-                        
+                        nx = cur_x + dx[d]
+                        ny = cur_y + dy[d]
                         if 0 <= nx < n and 0 <= ny < m:
                             if land[nx][ny] == 1 and not visited[nx][ny]:
-                                visited[nx][ny] = True
                                 queue.append((nx, ny))
-
+                                visited[nx][ny] = True
+            
                 for c in cols:
                     col_sum[c] += size
-                    
-                    
+    
     return max(col_sum)
