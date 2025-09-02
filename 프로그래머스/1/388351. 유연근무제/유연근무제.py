b9313159e1eaae0e56fc0e31bd9960f6dda2ec50
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-09-02 20:47:53 +0900

    [level 1] Title: 유연근무제, Time: 2.67 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -2,23 +2,27 @@ def solution(schedules, timelogs, startday):
     answer = 0
     
     for i in range(len(schedules)):
-        schedule = convert(schedules[i]) + 10
+        schedule = time_to_minute(schedules[i]) + 10
         timelog = timelogs[i]
         cur_day = startday - 1
-        
+        late = False
+
         for log in timelog:
             cur_day += 1
+            
             if cur_day % 7 in (0, 6):
                 continue
-        
-            if convert(log) > schedule:
+
+            if time_to_minute(log) > schedule:
+                late = True
                 break
-        else:
+
+        if not late:
             answer += 1
         
     return answer
             
-def convert(time):
-    time = str(time)
-    h, m = time[:-2], time[-2:]
-    return int(h) * 60 + int(m)
+def time_to_minute(time):
+    h = time // 100 # 앞에 두 개
+    m = time % 100  # 뒤에 두 개
+    return (h * 60) + m
