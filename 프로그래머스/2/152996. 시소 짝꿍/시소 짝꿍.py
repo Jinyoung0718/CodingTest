c4893a2cc7383a8e57590c12f7584bac3c7ac2e8
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-09-03 13:26:45 +0900

    [level 2] Title: 시소 짝꿍, Time: 9.59 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.py
@@ -1,22 +1,23 @@
 from collections import Counter
-from itertools import combinations
+
 
 def solution(weights):
     answer = 0
-    count = Counter(weights)
-    distances = [2, 3, 4]
-
-    for w in count:
-        if count[w] > 1:
-            # n개 중에 2개(짝)을 구할 수 있는 경우의 수 -> nC2
-            answer += count[w] * (count[w] - 1) // 2
     
-    # 다른 무게끼리 (거리 비율 맞는 경우)
-    for a, b in combinations(distances, 2):
-        for w in count:
-            #내 무게 w, 자리 거리 a에 앉았을 때 상대방이 b에 앉으면 그 사람이 가져야 할 무게가 바로 target
-            target = w * b / a
-            if target in count:
-                answer += count[w] * count[target]
+    counter = Counter(weights)
     
+    for k,v in counter.items():
+        if v>=2:
+            answer+= v*(v-1)//2
+    
+    weights = set(weights) 
+    
+    # 2:3 2:4 3:4 비율 가지면 짝궁 가능함
+    for w in weights:
+        if w*2/3 in weights:
+            answer+= counter[w*2/3] * counter[w]
+        if w*2/4 in weights:
+            answer+= counter[w*2/4] * counter[w]
+        if w*3/4 in weights:
+            answer+= counter[w*3/4] * counter[w]
     return answer
