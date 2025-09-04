2ca43399f4fcf23697f6ce508d83aec5fb913a57
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-09-04 23:50:47 +0900

    [level 2] Title: 지게차와 크레인, Time: 226.59 ms, Memory: 9.56 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.py
@@ -1,41 +1,39 @@
 from collections import deque
 
-def all_side_bfs(graph, target):
-    for i in range(1, len(graph) - 1):
-        for j in range(1, len(graph[0]) - 1):
-            if graph[i][j] == target:
-                graph[i][j] = '.'
-
-def out_side_bfs(graph, target):
-    n, m = len(graph), len(graph[0])
-
-    queue = deque()
-    queue.append((0, 0))
-    visited = [[False] * m for _ in range(n)]
-    visited[0][0] = True
-    removed_set = set()
-    
-    dx = [-1, 0, 1, 0]
-    dy = [0, -1, 0, 1]
-    
-    while queue:
-        x, y = queue.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-                visited[nx][ny] = True
-                
-                if graph[nx][ny] == '.':
-                    queue.append((nx, ny))
-                    
-                elif graph[nx][ny] == target:
-                    removed_set.add((nx, ny))
-    
-    for i, j in removed_set:
-        graph[i][j] = '.'
-
 def solution(storage, requests):
+    
+    def all_side_bfs(graph, target):
+        for i in range(1, len(graph) - 1):
+            for j in range(1, len(graph[0]) - 1):
+                if graph[i][j] == target:
+                    graph[i][j] = '.'
+
+    def out_side_bfs(graph, target):
+        n, m = len(graph), len(graph[0])
+        queue = deque()
+        queue.append((0, 0))
+        visited = set()
+        visited.add((0, 0))
+        removed_set = set()
+        
+        dx = [-1, 0, 1, 0]
+        dy = [0, -1, 0, 1]
+        
+        while queue:
+            x, y = queue.popleft()
+            for d in range(4):
+                nx, ny = x + dx[d], y + dy[d]
+                if 0 <= nx < n and 0 <= ny < m and (nx, ny) not in visited:
+                    visited.add((nx, ny))
+                    if graph[nx][ny] == '.':
+                        queue.append((nx, ny))
+                    elif graph[nx][ny] == target:
+                        removed_set.add((nx, ny))
+        
+        for i, j in removed_set:
+            graph[i][j] = '.'
+            
+
     answer = 0
     rows, cols = len(storage), len(storage[0])
     graph = [['.'] * (cols + 2)]
