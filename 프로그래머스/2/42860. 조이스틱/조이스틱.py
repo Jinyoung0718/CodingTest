9a9934bb22b190fa7d62ba381ffbdd262a67af20
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-09-06 14:00:10 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,18 +1,22 @@
 def solution(name):
-    answer = 0 
+    answer = 0
     n = len(name)
-    move = n - 1
     
     for char in name:
         answer += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
     
-    for i in range(n):
+    move = n - 1
+    
+    for i in range(n): # “턴 지점 후보” 를 전부 시도하는 루프
         next_i = i + 1
         
+        # 그 후보(i)에 대해 “i 다음에 A가 얼마나 길게 이어지나?” 측정
         while next_i < n and name[next_i] == 'A':
             next_i += 1
-        
+
         move = min(move, i * 2 + (n - next_i), (n - next_i) * 2 + i)
     
-    answer += move
+    
+    
+    answer += move    
     return answer
