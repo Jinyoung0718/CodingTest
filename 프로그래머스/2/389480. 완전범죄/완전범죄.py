ff8a21974a09f6cb25d3e1747f14a2596b97df2c
SoJinYoung <121544511+Jinyoung0718@users.noreply.github.com>
2025-09-07 16:59:37 +0900

    [level 2] Title: 완전범죄, Time: 1.09 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -3,16 +3,21 @@ def solution(infos, n, m):
 
     # dp[a] = A가 a 흔적일 때 B의 최소 흔적
     # dp[2] = 3 → 지금까지 A가 흔적 2를 남겼을 때, 그때까지 B는 최소 3 흔적을 남긴 상태
-    dp = [INF] * (n)
+    dp = ([INF] * n)
     dp[0] = 0
 
     for info in infos:
         a_trace, b_trace = info
+        
         # 이번 물건을 고려했을 때의 새로운 결과
         new_dp = [INF] * n
 
-        # A 도둑은 최대 n-1 흔적까지만 허용됨 -> 지금까지 A가 남긴 흔적 개수
+        # info 배열에 있는 a만을 대상으로 누적 a의 흔적을 인덱스로 하여금 b의 흔적을 계산
         for a in range(n):
+            
+            # info 배열에 없는 a는 건너 뜀
+            if dp[a] == INF:
+                continue
 
             # A가 훔치는 경우
             if a + a_trace < n:
@@ -32,8 +37,6 @@ def solution(infos, n, m):
 
     # 후보가 있으면 그중 최소값, 없으면 INF
     if candidates:
-        answer = min(candidates)
-    else:
-        answer = INF
-
-    return -1 if answer == INF else answer
+        return min(candidates)
+    
+    return -1
